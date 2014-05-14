70f7be9c7fbda3adad38a2fd9051dfbfb908583a
Éric Lemoine <eric.lemoine@gmail.com>
2014-05-14 22:30:04 +0200

    Add a simple example

---
+++ examples/simple.js
@@ -0,0 +1,29 @@
+(function() {
+  goog.provide('app');
+
+  goog.require('ga_map');
+  goog.require('ol.Map');
+  goog.require('ol.layer.Tile');
+  goog.require('ol.source.OSM');
+  goog.require('ol.View2D');
+
+  var module = angular.module('app', [
+    'go_map_directive'
+  ]);
+
+  module.controller('MainController', ['$scope', function($scope) {
+      $scope.map = new ol.Map({
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View2D({
+          center: [0, 0],
+          zoom: 4
+        })
+      });
+    }]);
+
+})();
+
