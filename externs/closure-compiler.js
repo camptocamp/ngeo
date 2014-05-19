b2abc4e825f3752bb13426edda26bf228c1e61cc
Éric Lemoine <eric.lemoine@gmail.com>
2014-05-19 18:00:35 +0200

    Add closure-compiler.js externs file

---
+++ externs/closure-compiler.js
@@ -0,0 +1,77 @@
+/**
+ * @fileoverview Definitions for externs that are either missing or incorrect
+ * in the current release version of the closure compiler we use.
+ *
+ * The entries must be removed once they are available/correct in the
+ * version we use.
+ *
+ * @externs
+ */
+
+// @see https://code.google.com/p/closure-compiler/issues/detail?id=1060
+
+
+/** @type {Date} */
+HTMLInputElement.prototype.valueAsDate;
+
+
+/** @type {number} */
+HTMLInputElement.prototype.valueAsNumber;
+
+
+// @see https://code.google.com/p/closure-compiler/issues/detail?id=1084
+
+
+/** @type {?number} */
+DeviceRotationRate.prototype.alpha;
+
+
+/** @type {?number} */
+DeviceRotationRate.prototype.beta;
+
+
+/** @type {?number} */
+DeviceRotationRate.prototype.gamma;
+
+
+// @see https://code.google.com/p/closure-compiler/issues/detail?id=1088
+
+
+/** @type {?number} */
+DeviceOrientationEvent.prototype.webkitCompassAccuracy;
+
+
+/** @type {?number} */
+DeviceOrientationEvent.prototype.webkitCompassHeading;
+
+
+/** @type {boolean} */
+Document.prototype.msFullscreenEnabled;
+
+
+/** @type {Element} */
+Document.prototype.msFullscreenElement;
+
+
+/** @type {number} */
+Touch.prototype.force;
+
+
+/** @type {number} */
+Touch.prototype.radiusX;
+
+
+/** @type {number} */
+Touch.prototype.radiusY;
+
+
+/** @type {number} */
+Touch.prototype.webkitForce;
+
+
+/** @type {number} */
+Touch.prototype.webkitRadiusX;
+
+
+/** @type {number} */
+Touch.prototype.webkitRadiusY;
