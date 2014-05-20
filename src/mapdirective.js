707a12c43bfe6de35745dc2044ac86e0f51c4d22
Éric Lemoine <eric.lemoine@gmail.com>
2014-05-20 10:22:13 +0200

    Better type checking in map directive

--- src/mapdirective.js
+++ src/mapdirective.js
@@ -1,18 +1,31 @@
 goog.provide('go_map_directive');
 
-(function() {
+goog.require('goog.object');
 
+(function() {
   var module = angular.module('go_map_directive', []);
 
-  module.directive('goMap',  function() {
-    return {
-      restrict: 'A',
-      scope: {
-        map: '=goMapMap'
-      },
-      link: function(scope, element, attrs) {
-        scope.map.setTarget(element[0]);
-      }
-    };
-  });
+  module.directive('goMap',
+      /**
+       * @return {angular.Directive} The directive specs.
+       */
+      function() {
+        return {
+          restrict: 'A',
+          scope: {
+            'm': '=goMapMap'
+          },
+          link:
+              /**
+               * @param {angular.Scope} scope Scope.
+               * @param {angular.JQLite} element Element.
+               * @param {angular.Attributes} attrs Attributes.
+               */
+              function(scope, element, attrs) {
+                /** @type {ol.Map} */
+                var map = goog.object.get(scope, 'm');
+                map.setTarget(element[0]);
+              }
+        };
+      });
 })();
