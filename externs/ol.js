0267b89da8f9ac0df92932b7c28626e0c601c585
Éric Lemoine <eric.lemoine@gmail.com>
2014-05-21 19:21:31 +0200

    Add externs/ol.js

---
+++ externs/ol.js
@@ -0,0 +1,21 @@
+/**
+ * @externs
+ */
+
+
+/**
+ * @type {Object}
+ */
+var ol = {};
+
+
+/**
+ * @constructor
+ */
+ol.Map = function() {};
+
+
+/**
+ * @param {Element|string|undefined} target
+ */
+ol.Map.prototype.setTarget = function(target) {};
