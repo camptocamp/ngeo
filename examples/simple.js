7e1908eeaaf5d509ddcf5650205a12b149fd839d
Éric Lemoine <eric.lemoine@gmail.com>
2014-05-24 23:00:56 +0200

    Fix lint errors in simple example

--- examples/simple.js
+++ examples/simple.js
@@ -2,9 +2,9 @@ goog.provide('simple');
 
 goog.require('go_map_directive');
 goog.require('ol.Map');
+goog.require('ol.View2D');
 goog.require('ol.layer.Tile');
 goog.require('ol.source.OSM');
-goog.require('ol.View2D');
 
 (function() {
   var module = angular.module('app', [
@@ -12,23 +12,22 @@ goog.require('ol.View2D');
   ]);
 
   module.controller('MainController', ['$scope',
-      /**
-       * @param {angular.Scope} $scope Scope.
-       */
-      function($scope) {
-          /** @type {ol.Map} */
-          $scope.map = new ol.Map({
-            layers: [
-              new ol.layer.Tile({
-                source: new ol.source.OSM()
-              })
-            ],
-            view: new ol.View2D({
-              center: [0, 0],
-              zoom: 4
-            })
-          });
-        }]);
+    /**
+     * @param {angular.Scope} $scope Scope.
+     */
+    function($scope) {
+      /** @type {ol.Map} */
+      $scope.map = new ol.Map({
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View2D({
+          center: [0, 0],
+          zoom: 4
+        })
+      });
+    }]);
 
 })();
-
