1aa156ca803723d6421fb68501d9fe0ff53ba783
Éric Lemoine <eric.lemoine@gmail.com>
2014-05-25 21:42:26 +0200

    Add a closure-util Serve script

---
+++ serve.js
@@ -0,0 +1,53 @@
+var path = require('path');
+var url = require('url');
+
+var closure = require('closure-util');
+var nomnom = require('nomnom');
+
+var log = closure.log;
+
+var options = nomnom.options({
+  port: {
+    abbr: 'p',
+    'default': 3000,
+    help: 'Port for incoming connections',
+    metavar: 'PORT'
+  },
+  loglevel: {
+    abbr: 'l',
+    choices: ['silly', 'verbose', 'info', 'warn', 'error'],
+    'default': 'info',
+    help: 'Log level',
+    metavar: 'LEVEL'
+  }
+}).parse();
+
+
+/** @type {string} */
+log.level = options.loglevel;
+
+log.info('ngeo', 'Parsing dependencies ...');
+var manager = new closure.Manager({
+  closure: true, // use the bundled Closure Library
+  lib: [
+    'src/**/*.js',
+    'node_modules/openlayers/src/**/*.js'
+  ],
+  main: 'examples/*.js'
+});
+manager.on('error', function(e) {
+  log.error('ngeo', e.message);
+});
+manager.on('ready', function() {
+  var server = new closure.Server({
+    manager: manager
+  });
+  server.listen(options.port, function() {
+    log.info('ngeo', 'Listening on http://localhost:' +
+        options.port + '/ (Ctrl+C to stop)');
+  });
+  server.on('error', function(err) {
+    log.error('ngeo', 'Server failed to start: ' + err.message);
+    process.exit(1);
+  });
+});
