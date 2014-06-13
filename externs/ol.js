dcacd416b9f9f4d881db42e2bf8ea95824276f64
Éric Lemoine <eric.lemoine@gmail.com>
2014-06-13 08:45:52 +0200

    Update externs/ol.js

--- externs/ol.js
+++ externs/ol.js
@@ -11,6 +11,13 @@ var ol = {};
 
 /**
  * @constructor
+ * @extends {ol.Object}
+ */
+ol.Collection = function() {};
+
+
+/**
+ * @constructor
  */
 ol.Map = function() {};
 
@@ -21,6 +28,28 @@ ol.Map.prototype.setTarget = function(target) {};
 
 
 /**
+ * @constructor
+ * @extends {ol.Observable}
+ */
+ol.Object = function() {};
+
+
+/**
+ * @constructor
+ */
+ol.Observable = function() {};
+
+
+/**
+ * @param {string|Array.<string>} type The event type or array of event types.
+ * @param {function(?): ?} listener The listener function.
+ * @param {Object=} opt_this The object to use as `this` in `listener`.
+ * @return {goog.events.Key} Unique key for the listener.
+ */
+ol.Observable.prototype.on = function(type, listener, opt_this) {};
+
+
+/**
  * @type {Object}
  */
 ol.layer = {};
@@ -28,6 +57,7 @@ ol.layer = {};
 
 /**
  * @constructor
+ * @extends {ol.Object}
  */
 ol.layer.Base = function() {};
 
