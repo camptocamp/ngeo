3bd824c94e4ed0b80e7abb8d051a7a3aa18c4a66
Éric Lemoine <eric.lemoine@gmail.com>
2014-06-13 08:27:28 +0200

    Make goMap a directive of type A

--- src/directives/mapdirective.js
+++ src/directives/mapdirective.js
@@ -2,29 +2,29 @@ goog.provide('go_map_directive');
 
 goog.require('go');
 
-goModule.directive('goMap',
-    /**
-     * @return {angular.Directive} The directive specs.
-     */
-    function() {
-      return {
-        restrict: 'E',
-        template: '<div></div>',
-        scope: {
-          'm': '=goMapMap'
-        },
-        link:
-            /**
-             * @param {angular.Scope} scope Scope.
-             * @param {angular.JQLite} element Element.
-             * @param {angular.Attributes} attrs Attributes.
-             */
-            function(scope, element, attrs) {
-              /** @type {ol.Map} */
-              var map = scope['m'];
-              goog.asserts.assertInstanceof(map, ol.Map);
+goModule.directive('goMap', ['goDefaultMap',
+  /**
+   * @param {string} goDefaultMap Default map constant.
+   * @return {angular.Directive} The directive specs.
+   */
+  function(goDefaultMap) {
+    return {
+      restrict: 'A',
+      link:
+          /**
+           * @param {angular.Scope} scope Scope.
+           * @param {angular.JQLite} element Element.
+           * @param {angular.Attributes} attrs Attributes.
+           */
+          function(scope, element, attrs) {
+            var attr = 'goMap';
+            var prop = attrs[attr] || goDefaultMap;
 
-              map.setTarget(element.children()[0]);
-            }
-      };
-    });
+            /** @type {ol.Map} */
+            var map = scope[prop];
+            goog.asserts.assertInstanceof(map, ol.Map);
+
+            map.setTarget(element[0]);
+          }
+    };
+  }]);
