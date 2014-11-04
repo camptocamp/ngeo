bdd01683e65b755802e13aa4bc74e1347b66b6fa
Éric Lemoine <eric.lemoine@gmail.com>
2014-11-04 14:54:35 +0100

    A different way to create an app-specific map

--- examples/map-extension.js
+++ examples/map-extension.js
@@ -11,10 +11,8 @@ goog.require('ol.source.OSM');
   var module = angular.module('app', ['ngeo']);
 
   /**
-   * An application-specific map directive. The directive gets a reference
-   * to the map from the ngeo directive's controller. Such a directive can
-   * be used to update the browser URL when the map view states change for
-   * example.
+   * An application-specific map directive. This directive gets a reference
+   * to the map instance through the "app-map" attribute.
    */
   module.directive('appMap', [
     /**
@@ -22,28 +20,27 @@ goog.require('ol.source.OSM');
      */
     function() {
       return {
-        restrict: 'A',
-        require: 'ngeoMap',
-        link:
-            /**
-             * @param {angular.Scope} scope Scope.
-             * @param {angular.JQLite} element Element.
-             * @param {angular.Attributes} attrs Attributes.
-             * @param {ngeo.MapDirectiveController} controller Controller.
-             */
-            function(scope, element, attrs, controller) {
-              var map = controller.getMap();
-              // do something with map…
-              map.getView().on('propertychange',
-                  /**
-                   * @param {ol.ObjectEvent} e Object event.
-                   */
-                  function(e) {
-                    window.console.log('"' + e.key + '" event received');
-                  });
-            }
+        restrict: 'E',
+        scope: {
+          'map': '=appMap'
+        },
+        controller: function() {
+          var map = this['map'];
+          // do something with map…
+          map.getView().on('propertychange',
+              /**
+               * @param {ol.ObjectEvent} e Object event.
+               */
+              function(e) {
+                window.console.log('"' + e.key + '" event received');
+              });
+        },
+        controllerAs: 'ctrl',
+        bindToController: true,
+        template: '<div ngeo-map=ctrl.map></div>'
       };
     }]);
+
 
   module.controller('MainController', ['$scope',
     /**
@@ -51,7 +48,7 @@ goog.require('ol.source.OSM');
      */
     function($scope) {
       /** @type {ol.Map} */
-      $scope['map'] = new ol.Map({
+      this['map'] = new ol.Map({
         layers: [
           new ol.layer.Tile({
             source: new ol.source.OSM()
