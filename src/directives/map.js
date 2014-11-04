2559ad578138bc88b17d9514c25603b87099d877
Éric Lemoine <eric.lemoine@gmail.com>
2014-11-04 11:17:56 +0100

    Docs for the ngeo-map directive

--- src/directives/map.js
+++ src/directives/map.js
@@ -3,6 +3,23 @@ goog.provide('ngeo_map_directive');
 goog.require('goog.asserts');
 goog.require('ngeo');
 
+
+/**
+ * The "map" directive. Used to insert a user-defined OpenLayers map
+ * in the DOM. The directive does not create an isolate scope and it
+ * expects a map instance in the parent scope.
+ *
+ * Example:
+ *
+ * <div ngeo-map></div>
+ *
+ * In this case the map directive will assume that the name of the
+ * scope property including the map instance is "map". To specify
+ * that name use this:
+ *
+ * <div ngeo-map="map1"></div>
+ *
+ */
 ngeoModule.directive('ngeoMap', ['ngeoDefaultMap',
   /**
    * @param {string} ngeoDefaultMap Default map constant.
