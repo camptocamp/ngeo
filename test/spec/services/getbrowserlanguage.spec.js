9346415f98d71c7d830df6e2482cc847a3d3bf06
Éric Lemoine <eric.lemoine@gmail.com>
2014-11-04 12:14:56 +0100

    Add tests for ngeoGetBrowserLanguage

---
+++ test/spec/services/getbrowserlanguage.spec.js
@@ -0,0 +1,28 @@
+goog.require('ngeo_getbrowserlanguage_service');
+
+describe('ngeo_getbrowserlanguage_service', function() {
+  var win;
+  var ngeoGetBrowserLanguage;
+
+  beforeEach(function() {
+    win = {navigator: {}};
+    module(function($provide) {
+      $provide.value('$window', win);
+    });
+    inject(function($injector) {
+      ngeoGetBrowserLanguage = $injector.get('ngeoGetBrowserLanguage');
+    });
+  });
+
+  it('gets language from navigator.languages', function() {
+    win.navigator.languages = ["en-US", "zh-CN", "ja-JP", "fr-FR"];
+    var langCode = ngeoGetBrowserLanguage(['de', 'fr', 'it']);
+    expect(langCode).toBe('fr');
+  });
+
+  it('gets language from navigator.language', function() {
+    win.navigator.language = ["fr-FR"];
+    var langCode = ngeoGetBrowserLanguage(['de', 'fr', 'it']);
+    expect(langCode).toBe('fr');
+  });
+});
