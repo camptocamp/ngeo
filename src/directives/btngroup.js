17b2d479de463a47c8bb35f015eff5f5fd607cdf
Éric Lemoine <eric.lemoine@gmail.com>
2014-11-13 17:56:08 +0100

    Reformat the ngeoBtn code

--- src/directives/btngroup.js
+++ src/directives/btngroup.js
@@ -19,38 +19,41 @@ goog.require('ngeo');
  *
  * See the `ngeo-btn` directive for more information.
  */
-ngeoModule.directive('ngeoBtnGroup', function() {
-  return {
-    restrict: 'A',
-    controller: ['$scope',
-      function($scope) {
-        /** @type {Array.<function(!angular.Scope, boolean)>} */
-        var buttons = [];
+ngeoModule.directive('ngeoBtnGroup', [
+  /**
+   */
+  function() {
+    return {
+      restrict: 'A',
+      controller: ['$scope',
+        function($scope) {
+          /** @type {Array.<function(!angular.Scope, boolean)>} */
+          var buttons = [];
 
-        /**
-         * @param {number} index Index of the button in buttons array.
-         */
-        this.activate = function(index) {
-          goog.array.forEach(buttons,
-              function(s, i) {
-                if (i != index) {
-                  s($scope, false);
-                }
-              });
-        };
+          /**
+           * @param {number} index Index of the button in buttons array.
+           */
+          this.activate = function(index) {
+            goog.array.forEach(buttons,
+                function(s, i) {
+                  if (i != index) {
+                    s($scope, false);
+                  }
+                });
+          };
 
-        /**
-         * @param {function((!angular.Scope), *)} ngModelSet Setter.
-         * @return {number} Index of the pushed setter.
-         */
-        this.addButton = function(ngModelSet) {
-          buttons.push(ngModelSet);
-          return buttons.length - 1;
-        };
-      }
-    ]
-  };
-});
+          /**
+           * @param {function((!angular.Scope), *)} ngModelSet Setter.
+           * @return {number} Index of the pushed setter.
+           */
+          this.addButton = function(ngModelSet) {
+            buttons.push(ngModelSet);
+            return buttons.length - 1;
+          };
+        }
+      ]
+    };
+  }]);
 
 
 /**
@@ -77,48 +80,51 @@ ngeoModule.directive('ngeoBtnGroup', function() {
  * In that example the `ngeo-btn` are combined together in a "toggle group",
  * where activating a button will deactivate the others.
  */
-ngeoModule.directive('ngeoBtn', ['$parse', function($parse) {
-  return {
-    require: ['?^ngeoBtnGroup', 'ngModel'],
-    restrict: 'A',
-    link:
-        /**
-         * @param {angular.Scope} scope Scope.
-         * @param {angular.JQLite} element Element.
-         * @param {angular.Attributes} attrs Attributes.
-         * @param {!Array.<!Object>} ctrls Controllers.
-         */
-        function(scope, element, attrs, ctrls) {
-          var buttonsCtrl = ctrls[0];
-          var ngModelCtrl = ctrls[1];
-          var indexInGroup = -1;
+ngeoModule.directive('ngeoBtn', ['$parse',
+  /**
+   */
+  function($parse) {
+    return {
+      require: ['?^ngeoBtnGroup', 'ngModel'],
+      restrict: 'A',
+      link:
+          /**
+           * @param {angular.Scope} scope Scope.
+           * @param {angular.JQLite} element Element.
+           * @param {angular.Attributes} attrs Attributes.
+           * @param {!Array.<!Object>} ctrls Controllers.
+           */
+          function(scope, element, attrs, ctrls) {
+            var buttonsCtrl = ctrls[0];
+            var ngModelCtrl = ctrls[1];
+            var indexInGroup = -1;
 
-          var ngModelGet = $parse(attrs['ngModel']);
-          var ngModelSet = ngModelGet.assign;
+            var ngModelGet = $parse(attrs['ngModel']);
+            var ngModelSet = ngModelGet.assign;
 
-          // Set ng-model value to false if undefined
-          if (!goog.isDef(ngModelGet(scope))) {
-            ngModelSet(scope, false);
-          }
-          if (goog.isDef(buttonsCtrl)) {
-            indexInGroup = buttonsCtrl.addButton(ngModelSet);
-          }
-
-          // UI -> model
-          element.bind('click', function() {
-            scope.$apply(function() {
-              ngModelCtrl.$setViewValue(!ngModelCtrl.$viewValue);
-              ngModelCtrl.$render();
-            });
-          });
-
-          // model -> UI
-          ngModelCtrl.$render = function() {
-            if (ngModelCtrl.$viewValue && goog.isDef(buttonsCtrl)) {
-              buttonsCtrl.activate(indexInGroup);
+            // Set ng-model value to false if undefined
+            if (!goog.isDef(ngModelGet(scope))) {
+              ngModelSet(scope, false);
             }
-            element.toggleClass('active', ngModelCtrl.$viewValue);
-          };
-        }
-  };
-}]);
+            if (goog.isDef(buttonsCtrl)) {
+              indexInGroup = buttonsCtrl.addButton(ngModelSet);
+            }
+
+            // UI -> model
+            element.bind('click', function() {
+              scope.$apply(function() {
+                ngModelCtrl.$setViewValue(!ngModelCtrl.$viewValue);
+                ngModelCtrl.$render();
+              });
+            });
+
+            // model -> UI
+            ngModelCtrl.$render = function() {
+              if (ngModelCtrl.$viewValue && goog.isDef(buttonsCtrl)) {
+                buttonsCtrl.activate(indexInGroup);
+              }
+              element.toggleClass('active', ngModelCtrl.$viewValue);
+            };
+          }
+    };
+  }]);
