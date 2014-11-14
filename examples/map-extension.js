259e989fbb430760a8cbca8391b6fda4209d1d9f
Éric Lemoine <eric.lemoine@gmail.com>
2014-11-15 00:02:17 +0100

    Use location service in map-extension example

--- examples/map-extension.js
+++ examples/map-extension.js
@@ -1,5 +1,6 @@
 goog.provide('mapextension');
 
+goog.require('ngeo_location_service');
 goog.require('ngeo_map_directive');
 goog.require('ol.Map');
 goog.require('ol.View');
@@ -10,36 +11,76 @@ goog.require('ol.source.OSM');
   var module = angular.module('app', ['ngeo']);
 
   /**
-   * An application-specific map directive. This directive gets a reference
-   * to the map instance through the "app-map" attribute.
+   * An application-specific map directive that updates the URL in the browser
+   * address bar when the map view changes. It also sets the initial view based
+   * on the URL query params at init time.
+   *
+   * This directive gets a reference to the map instance through the "app-map"
+   * attribute.
    */
   module.directive('appMap', [
     /**
      * @return {angular.Directive} The directive specs.
      */
-    function() {
+    function(ngeoLocation) {
       return {
         restrict: 'E',
         scope: {
           'map': '=appMap'
         },
-        controller: function() {
-          var map = this['map'];
-          // do something with map…
-          map.getView().on('propertychange',
-              /**
-               * @param {ol.ObjectEvent} e Object event.
-               */
-              function(e) {
-                // we simply output the view property here, but we
-                // could update the browser URL for example…
-                window.console.log('"' + e.key + '" event received');
-              });
-        },
+        controller: 'AppMapController',
         controllerAs: 'ctrl',
         bindToController: true,
         template: '<div ngeo-map=ctrl.map></div>'
       };
+    }]);
+
+
+  /**
+   * The controller for the `appMap` directive.
+   */
+  module.controller('AppMapController', ['$scope', 'ngeoLocation',
+    /**
+     * @param {angular.Scope} $scope Scope.
+     * @param {ngeo.Location} ngeoLocation ngeo Location service.
+     */
+    function($scope, ngeoLocation) {
+      var map = this['map'];
+      var view = map.getView();
+
+      var zoom = ngeoLocation.getParam('z');
+      zoom = angular.isDefined(zoom) ? +zoom : 4;
+
+      var x = ngeoLocation.getParam('x');
+      var y = ngeoLocation.getParam('y');
+      var center = angular.isDefined(x) && angular.isDefined(y) ?
+          [+x, +y] : [0, 0];
+
+      view.setCenter(center);
+      view.setZoom(zoom);
+
+      ngeoLocation.updateParams({
+        'z': zoom,
+        'x': Math.round(center[0]),
+        'y': Math.round(center[1])
+      });
+
+      view.on('propertychange',
+          /**
+           * @param {ol.ObjectEvent} e Object event.
+           */
+          function(e) {
+            var center = view.getCenter();
+            var params = {
+              'z': view.getZoom(),
+              'x': Math.round(center[0]),
+              'y': Math.round(center[1])
+            };
+            $scope.$apply(function() {
+              ngeoLocation.updateParams(params);
+            });
+          });
+
     }]);
 
 
@@ -54,11 +95,7 @@ goog.require('ol.source.OSM');
           new ol.layer.Tile({
             source: new ol.source.OSM()
           })
-        ],
-        view: new ol.View({
-          center: [0, 0],
-          zoom: 4
-        })
+        ]
       });
     }]);
 
