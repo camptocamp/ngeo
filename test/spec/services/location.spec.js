e9edf75ebe29c177de6c22b6c2e3ac364b94ea15
Éric Lemoine <eric.lemoine@gmail.com>
2014-11-17 14:06:03 +0100

    Add tests for ngeoLocation service

---
+++ test/spec/services/location.spec.js
@@ -0,0 +1,54 @@
+goog.require('ngeo_location_service');
+
+describe('ngeo_location_service', function() {
+  var win;
+  var ngeoLocation;
+
+  beforeEach(function() {
+    win = {
+      'location': 'http://domain.com/some/path?some=param',
+      'history': {'replaceState': function() {}}
+    };
+    spyOn(win.history, 'replaceState');
+    module(function($provide) {
+      $provide.value('$window', win);
+    });
+    inject(function($injector) {
+      ngeoLocation = $injector.get('ngeoLocation');
+    });
+  });
+
+  describe('#getUri', function() {
+    it('returns the URI', function() {
+      var uri = ngeoLocation.getUri();
+      expect(uri).toBe('http://domain.com/some/path?some=param');
+    });
+    it('returns the URI with additional params', function() {
+      var uri = ngeoLocation.getUri({'another': 'param'});
+      expect(uri).toBe('http://domain.com/some/path?some=param&another=param');
+    });
+  });
+
+  describe('#getParam', function() {
+    it('returns the param value', function() {
+      var value = ngeoLocation.getParam('some');
+      expect(value).toBe('param');
+    });
+  });
+
+  describe('#deleteParam', function() {
+    it('delete the params', function() {
+      ngeoLocation.deleteParam('some');
+      var uri = ngeoLocation.getUri();
+      expect(uri).toBe('http://domain.com/some/path');
+    });
+  });
+
+  describe('#refresh', function() {
+    it('calls history.replaceState with expected args', function() {
+      ngeoLocation.refresh();
+      expect(win.history.replaceState).toHaveBeenCalledWith(
+        null, '', 'http://domain.com/some/path?some=param');
+    });
+  });
+});
