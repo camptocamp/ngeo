84249cdc1ecd7c12062bfdc04328cc6b4af854d0
Éric Lemoine <eric.lemoine@gmail.com>
2014-11-24 22:04:54 +0100

    New style guide for opacity example

--- examples/layeropacity.js
+++ examples/layeropacity.js
@@ -1,36 +1,44 @@
 goog.provide('layeropacity');
 
-goog.require('ngeo_decoratelayer_service');
-goog.require('ngeo_map_directive');
+goog.require('ngeo.DecorateLayer');
+goog.require('ngeo.mapDirective');
 goog.require('ol.Map');
 goog.require('ol.View');
 goog.require('ol.layer.Tile');
 goog.require('ol.source.OSM');
 
-(function() {
-  var module = angular.module('app', ['ngeo']);
 
-  module.controller('MainController', ['$scope', 'ngeoDecorateLayer',
-    /**
-     * @param {angular.Scope} $scope Scope.
-     * @param {ngeo.DecorateLayer} ngeoDecorateLayer Decorate layer service.
-     */
-    function($scope, ngeoDecorateLayer) {
+/** @const **/
+var app = {};
 
-      /** @type {ol.layer.Tile} */
-      var layer = new ol.layer.Tile({
-        source: new ol.source.OSM()
-      });
-      ngeoDecorateLayer(layer);
-      this['layer'] = layer;
 
-      /** @type {ol.Map} */
-      $scope['map'] = new ol.Map({
-        layers: [layer],
-        view: new ol.View({
-          center: [0, 0],
-          zoom: 4
-        })
-      });
-    }]);
-})();
+var module = angular.module('app', ['ngeo']);
+
+
+
+/**
+ * @param {angular.Scope} $scope Scope.
+ * @param {ngeo.DecorateLayer} ngeoDecorateLayer Decorate layer service.
+ * @constructor
+ * @ngInject
+ */
+app.MainController = function($scope, ngeoDecorateLayer) {
+  /** @type {ol.layer.Tile} */
+  var layer = new ol.layer.Tile({
+    source: new ol.source.OSM()
+  });
+  ngeoDecorateLayer(layer);
+  this['layer'] = layer;
+
+  /** @type {ol.Map} */
+  this['map'] = new ol.Map({
+    layers: [layer],
+    view: new ol.View({
+      center: [0, 0],
+      zoom: 4
+    })
+  });
+};
+
+
+module.controller('MainController', app.MainController);
