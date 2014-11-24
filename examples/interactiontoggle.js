efc67e8d069639f1a3b425c0824de2bcf36a693e
Éric Lemoine <eric.lemoine@gmail.com>
2014-11-24 21:39:37 +0100

    New style guide for interactiontoggle example

--- examples/interactiontoggle.js
+++ examples/interactiontoggle.js
@@ -1,7 +1,7 @@
 goog.provide('interactiontoggle');
 
-goog.require('ngeo_decorateinteraction_service');
-goog.require('ngeo_map_directive');
+goog.require('ngeo.DecorateInteraction');
+goog.require('ngeo.mapDirective');
 goog.require('ol.FeatureOverlay');
 goog.require('ol.Map');
 goog.require('ol.View');
@@ -9,44 +9,53 @@ goog.require('ol.interaction.Draw');
 goog.require('ol.layer.Tile');
 goog.require('ol.source.MapQuest');
 
-(function() {
-  var module = angular.module('app', ['ngeo']);
 
-  module.controller('MainController', ['$scope', 'ngeoDecorateInteraction',
-    /**
-     * @param {angular.Scope} $scope Scope.
-     * @param {ngeo.DecorateInteraction} ngeoDecorateInteraction Decorate
-     *     interaction service.
-     */
-    function($scope, ngeoDecorateInteraction) {
+/** @const **/
+var app = {};
 
-      /** @type {ol.Map} */
-      var map = new ol.Map({
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.MapQuest({layer: 'sat'})
-          })
-        ],
-        view: new ol.View({
-          center: [-10997148, 4569099],
-          zoom: 4
-        })
-      });
-      $scope['map'] = map;
 
-      var featureOverlay = new ol.FeatureOverlay();
-      featureOverlay.setMap(map);
+var module = angular.module('app', ['ngeo']);
 
-      /** @type {ol.interaction.Draw} */
-      var interaction = new ol.interaction.Draw(
-          /** @type {olx.interaction.DrawOptions} */ ({
-            type: 'Point',
-            features: featureOverlay.getFeatures()
-          }));
-      interaction.setActive(false);
-      map.addInteraction(interaction);
-      ngeoDecorateInteraction(interaction);
-      this['interaction'] = interaction;
 
-    }]);
-})();
+
+/**
+ * @param {angular.Scope} $scope Scope.
+ * @param {ngeo.DecorateInteraction} ngeoDecorateInteraction Decorate
+ *     interaction service.
+ * @constructor
+ * @ngInject
+ */
+app.MainController = function($scope, ngeoDecorateInteraction) {
+
+  /** @type {ol.Map} */
+  var map = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.MapQuest({layer: 'sat'})
+      })
+    ],
+    view: new ol.View({
+      center: [-10997148, 4569099],
+      zoom: 4
+    })
+  });
+  this['map'] = map;
+
+  var featureOverlay = new ol.FeatureOverlay();
+  featureOverlay.setMap(map);
+
+  /** @type {ol.interaction.Draw} */
+  var interaction = new ol.interaction.Draw(
+      /** @type {olx.interaction.DrawOptions} */ ({
+        type: 'Point',
+        features: featureOverlay.getFeatures()
+      }));
+  interaction.setActive(false);
+  map.addInteraction(interaction);
+  ngeoDecorateInteraction(interaction);
+  this['interaction'] = interaction;
+
+};
+
+
+module.controller('MainController', app.MainController);
