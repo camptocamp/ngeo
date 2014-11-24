68065530072fe6bd05f59c6df6739d37570433dc
Éric Lemoine <eric.lemoine@gmail.com>
2014-11-24 22:07:13 +0100

    New style guide for visibility example

--- examples/layervisibility.js
+++ examples/layervisibility.js
@@ -1,47 +1,56 @@
 goog.provide('layervisibility');
 
-goog.require('ngeo_decoratelayer_service');
-goog.require('ngeo_map_directive');
+goog.require('ngeo.DecorateLayer');
+goog.require('ngeo.mapDirective');
 goog.require('ol.Map');
 goog.require('ol.View');
 goog.require('ol.layer.Tile');
 goog.require('ol.source.MapQuest');
 goog.require('ol.source.TileWMS');
 
-(function() {
-  var module = angular.module('app', ['ngeo']);
 
-  module.controller('MainController', ['$scope', 'ngeoDecorateLayer',
-    /**
-     * @param {angular.Scope} $scope Scope.
-     * @param {ngeo.DecorateLayer} ngeoDecorateLayer Decorate layer service.
-     */
-    function($scope, ngeoDecorateLayer) {
+/** @const **/
+var app = {};
 
-      /** @type {ol.layer.Tile} */
-      var wmsLayer = new ol.layer.Tile({
-        source: new ol.source.TileWMS({
-          url: 'http://demo.opengeo.org/geoserver/wms',
-          params: {'LAYERS': 'topp:states'},
-          serverType: 'geoserver',
-          extent: [-13884991, 2870341, -7455066, 6338219]
-        })
-      });
-      ngeoDecorateLayer(wmsLayer);
-      this['layer'] = wmsLayer;
 
-      /** @type {ol.Map} */
-      $scope['map'] = new ol.Map({
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.MapQuest({layer: 'sat'})
-          }),
-          wmsLayer
-        ],
-        view: new ol.View({
-          center: [-10997148, 4569099],
-          zoom: 4
-        })
-      });
-    }]);
-})();
+var module = angular.module('app', ['ngeo']);
+
+
+
+/**
+ * @param {angular.Scope} $scope Scope.
+ * @param {ngeo.DecorateLayer} ngeoDecorateLayer Decorate layer service.
+ * @constructor
+ * @ngInject
+ */
+app.MainController = function($scope, ngeoDecorateLayer) {
+
+  /** @type {ol.layer.Tile} */
+  var wmsLayer = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url: 'http://demo.opengeo.org/geoserver/wms',
+      params: {'LAYERS': 'topp:states'},
+      serverType: 'geoserver',
+      extent: [-13884991, 2870341, -7455066, 6338219]
+    })
+  });
+  ngeoDecorateLayer(wmsLayer);
+  this['layer'] = wmsLayer;
+
+  /** @type {ol.Map} */
+  this['map'] = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.MapQuest({layer: 'sat'})
+      }),
+      wmsLayer
+    ],
+    view: new ol.View({
+      center: [-10997148, 4569099],
+      zoom: 4
+    })
+  });
+};
+
+
+module.controller('MainController', app.MainController);
