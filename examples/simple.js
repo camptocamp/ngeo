8c1e78f82c66500525e727953907f3664d740bc7
Éric Lemoine <eric.lemoine@gmail.com>
2014-11-24 21:12:14 +0100

    New style guide for simple example

--- examples/simple.js
+++ examples/simple.js
@@ -1,31 +1,41 @@
 goog.provide('simple');
 
-goog.require('ngeo_map_directive');
+goog.require('ngeo.mapDirective');
 goog.require('ol.Map');
 goog.require('ol.View');
 goog.require('ol.layer.Tile');
 goog.require('ol.source.OSM');
 
-(function() {
-  var module = angular.module('app', ['ngeo']);
 
-  module.controller('MainController', ['$scope',
-    /**
-     * @param {angular.Scope} $scope Scope.
-     */
-    function($scope) {
-      /** @type {ol.Map} */
-      $scope['map'] = new ol.Map({
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-          center: [0, 0],
-          zoom: 4
-        })
-      });
-    }]);
+/** @const **/
+var app = {};
 
-})();
+
+var module = angular.module('app', ['ngeo']);
+
+
+
+/**
+ * @param {angular.Scope} $scope Scope.
+ * @constructor
+ * @ngInject
+ */
+app.MainController = function($scope) {
+  /**
+   * @type {ol.Map}
+   */
+  this['map'] = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: [0, 0],
+      zoom: 4
+    })
+  });
+};
+
+
+module.controller('MainController', app.MainController);
