eed5fdfe2a6ec2f9e0de611b2bb777d2e2b4fbb1
Éric Lemoine <eric.lemoine@gmail.com>
2014-11-25 15:38:10 +0100

    Add a resizemap directive

---
+++ src/directives/resizemap.js
@@ -0,0 +1,62 @@
+/**
+ * @fileoverview Provides a directive to use on resizable elements that change
+ * the map size when they are resized. The directive calls `map.updateSize`
+ * during the animation.
+ *
+ * Example:
+ *
+ *   <div ng-class="ctrl.open ? 'open' : 'close' ngeo-resizemap="ctrl.map"><div>
+ *   <input type="checkbox" ng-model="ctrl.open" />
+ */
+
+goog.provide('ngeo.resizemapDirective');
+
+goog.require('goog.async.AnimationDelay');
+goog.require('ngeo');
+goog.require('ol.Map');
+
+
+/**
+ * @param {angular.$window} $window Angular window service.
+ * @return {angular.Directive} The directive specs.
+ * @ngInject
+ */
+ngeo.resizemapDirective = function($window) {
+  return {
+    restrict: 'A',
+    link:
+        /**
+         * @param {angular.Scope} scope Scope.
+         * @param {angular.JQLite} element Element.
+         * @param {angular.Attributes} attrs Attributes.
+         */
+        function(scope, element, attrs) {
+          var attr = 'ngeoResizemap';
+          var prop = attrs[attr];
+          var map = scope.$eval(prop);
+          goog.asserts.assertInstanceof(map, ol.Map);
+
+          var animationDelay = new goog.async.AnimationDelay(
+              /**
+               * @param {number} time Time.
+               */
+              function(time) {
+                map.updateSize();
+                animationDelay.start();
+              }, $window);
+
+          element.on('$animate:before', function() {
+            animationDelay.stop();
+            animationDelay.start();
+          });
+
+          element.on('$animate:close', function() {
+            map.updateSize();
+            animationDelay.stop();
+          });
+        }
+  };
+};
+
+
+ngeoModule.directive('ngeoResizemap', ngeo.resizemapDirective);
