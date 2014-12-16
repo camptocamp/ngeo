11ab253046b1f8a962d76b2ef67e3efc57538e82
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2014-12-16 17:09:59 +0100

    Adding externs for bootstrap tab & modal

---
+++ externs/twbootstrap.js
@@ -0,0 +1,19 @@
+/**
+ * @fileoverview Externs for Twitter Bootstrap
+ *
+ * @externs
+ */
+
+
+/**
+* @param {string|Object.<string,*>|undefined} opt_options
+* @return {jQuery}
+*/
+jQuery.prototype.modal = function(opt_options) {};
+
+
+/**
+ * @param {string} action
+ * @return {!jQuery}
+ */
+jQuery.prototype.tab = function(action) {};
