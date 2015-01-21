d89c4adf15ad3ef54e98f811a03a3e9b463c96ea
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-01-21 16:18:57 +0100

    Adding a sortable directive and use to change layer order

---
+++ examples/layerorder.js
@@ -0,0 +1,97 @@
+goog.provide('layerorder');
+
+goog.require('ngeo.DecorateLayer');
+goog.require('ngeo.mapDirective');
+goog.require('ngeo.sortableDirective');
+goog.require('ol.Map');
+goog.require('ol.View');
+goog.require('ol.layer.Tile');
+goog.require('ol.source.MapQuest');
+goog.require('ol.source.TileWMS');
+
+
+/** @const **/
+var app = {};
+
+
+/** @type {!angular.Module} **/
+app.module = angular.module('app', ['ngeo']);
+
+
+
+/**
+ * @param {angular.Scope} $scope Scope.
+ * @param {ngeo.DecorateLayer} ngeoDecorateLayer Decorate layer service.
+ * @constructor
+ * @ngInject
+ */
+app.MainController = function($scope, ngeoDecorateLayer) {
+
+  /** @type {ol.layer.Tile} */
+  var boundaries = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url: 'http://demo.opengeo.org/geoserver/wms',
+      params: {'LAYERS': 'topp:tasmania_state_boundaries'},
+      serverType: 'geoserver'
+    }),
+    name: 'Boundaries'
+  });
+
+  /** @type {ol.layer.Tile} */
+  var roads = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url: 'http://demo.opengeo.org/geoserver/wms',
+      params: {'LAYERS': 'topp:tasmania_roads'},
+      serverType: 'geoserver'
+    }),
+    name: 'Roads'
+  });
+
+  /** @type {ol.layer.Tile} */
+  var waterBodies = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url: 'http://demo.opengeo.org/geoserver/wms',
+      params: {'LAYERS': 'topp:tasmania_water_bodies'},
+      serverType: 'geoserver'
+    }),
+    name: 'Water bodies'
+  });
+
+  /** @type {ol.layer.Tile} */
+  var cities = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url: 'http://demo.opengeo.org/geoserver/wms',
+      params: {'LAYERS': 'topp:tasmania_cities'},
+      serverType: 'geoserver'
+    }),
+    name: 'Cities'
+  });
+
+
+  /** @type {ol.Map} */
+  this['map'] = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.MapQuest({layer: 'sat'}),
+        name: 'MapQuest'
+      }),
+      boundaries,
+      roads,
+      waterBodies,
+      cities
+    ],
+    view: new ol.View({
+      center: [16339075, -5194965],
+      zoom: 7
+    })
+  });
+
+  // watch any change on layers array to refresh the map
+  $scope.layers = this['map'].getLayers().getArray();
+  $scope.$watchCollection('layers', goog.bind(function() {
+    this['map'].render();
+  }, this));
+};
+
+
+app.module.controller('MainController', app.MainController);
---
+++ src/directives/sortable.js
@@ -0,0 +1,91 @@
+/**
+ * @fileoverview Provides a directive that can be used to change the order of
+ * items in an array.
+ * It can be used to change the order of layers in the map for example.
+ * It requires JQuery-UI Sortable.
+ *
+ * Example:
+ *
+ * <ul ngeo-sortable="ctrl.map.getLayers().getArray()">
+ *   <li ng-repeat="ctrl.map.getLayers().getArray()">
+ *      <span class="handle"></span>{{layer.get('name')}}
+ *   </li>
+ * </ul>
+ */
+
+goog.provide('ngeo.sortableDirective');
+
+goog.require('ngeo');
+
+
+/**
+ * @param {angular.$log} $log Angular log service.
+ * @param {angular.$timeout} $timeout Angular timeout service.
+ * @return {angular.Directive} The directive specs.
+ * @ngInject
+ */
+ngeo.sortableDirective = function($log, $timeout) {
+  return {
+    restrict: 'A',
+    scope: true,
+    link:
+        /**
+         * @param {angular.Scope} scope Scope.
+         * @param {angular.JQLite} element Element.
+         * @param {angular.Attributes} attrs Attributes.
+         */
+        function(scope, element, attrs) {
+          var attr = 'ngeoSortable';
+          var prop = attrs[attr];
+
+          var sortable = /** @type {Array} */ (scope.$eval(prop));
+
+          if (!angular.element.fn || !angular.element.fn.jquery) {
+            $log.error('ui.sortable: jQuery should be included before' +
+                'AngularJS!');
+            return;
+          }
+
+          var startIndex;
+
+          // When we add or remove elements, we need the sortable to 'refresh'
+          // so it can find the new/removed elements.
+          scope.$watch('sortable.length', function() {
+            // Timeout to let ng-repeat modify the DOM
+            $timeout(function() {
+              element.sortable('refresh');
+            }, 0, false);
+          });
+
+
+          var callbacks = {};
+          callbacks.start = function(e, ui) {
+            // Save the starting position of dragged item
+            startIndex = ui.item.index();
+          };
+
+          callbacks.update = function(e, ui) {
+            scope.$apply(function() {
+              sortable.splice(
+                  ui.item.index(), 0,
+                  sortable.splice(startIndex, 1)[0]);
+            });
+            startIndex = undefined;
+          };
+
+
+          element.sortable();
+
+          angular.forEach(callbacks, function(value, key) {
+            element.sortable('option', key, value);
+          });
+
+          element.sortable('option', 'handle', '.handle');
+          element.sortable('option', 'containment', 'parent');
+          element.sortable('option', 'opacity', 0.5);
+          element.sortable('option', 'cursor', 'move');
+        }
+  };
+};
+
+ngeoModule.directive('ngeoSortable', ngeo.sortableDirective);
