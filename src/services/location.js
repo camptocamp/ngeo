f2cd74305367c48c798b2e0f3203086bac072867
Éric Lemoine <eric.lemoine@gmail.com>
2015-02-05 14:37:46 +0100

    Document incompat between ngeoLocation and $location

--- src/services/location.js
+++ src/services/location.js
@@ -1,13 +1,33 @@
 /**
  * @fileoverview Provides a service for interacting with the URL in the
  * browser address bar.
+ *
+ * WARNING: because of a bug in Angular this service is not compatible with
+ * the $location service. This further means that service is not compatible
+ * with the $anchorScroll and $route services, and with the ng-include and
+ * ng-view directives (which are based on the $anchorScroll and $route
+ * services). See <https://github.com/angular/angular.js/issues/1417>.
+ *
+ * This file also provides an ngeo.mockLocationProvider function that you can
+ * use to mock Angular's $location provider and make it possible to use both
+ * ngeoLocation and ng-include.
+ *
+ * app.module.config(ngeo.mockLocationProvider);
+ *
  */
 
 goog.provide('ngeo.Location');
+goog.provide('ngeo.MockLocationProvider');
 
 goog.require('goog.Uri');
 goog.require('goog.object');
 goog.require('ngeo');
+
+
+/**
+ * @typedef {function(angular.$locationProvider)}
+ */
+ngeo.MockLocationProvider;
 
 
 
@@ -114,3 +134,51 @@ ngeo.LocationFactory = function($rootScope, $window) {
 
 
 ngeoModule.factory('ngeoLocation', ngeo.LocationFactory);
+
+
+/**
+ * A function that changes Angular's $location provider to avoid problem
+ * when both ngeoLocation and $location are used in an application. This
+ * is how you can use that function in an application:
+ *
+ * app.module.config(ngeo.mockLocationProvider);
+ *
+ * @param {angular.$locationProvider} $locationProvider Angular location
+ *     provider.
+ */
+ngeo.mockLocationProvider = function($locationProvider) {
+  /**
+   * @return {angular.$location} Mock object for Angular location service.
+   */
+  $locationProvider['$get'] = function() {
+    var locationMock = /** @type {angular.$location} */ ({
+      absUrl: function() {
+        return '';
+      },
+      hash: function(opt_path) {
+        return goog.isDef(opt_path) ? this : '';
+      },
+      host: function() {
+        return '';
+      },
+      path: function(opt_path) {
+        return goog.isDef(opt_path) ? this : '';
+      },
+      port: function() {
+        return 0;
+      },
+      protocol: function() {
+        return '';
+      },
+      replace: function() {
+      },
+      search: function(opt_search, opt_paramValue) {
+        return goog.isDef(opt_search) ? this : {};
+      },
+      url: function(opt_url) {
+        return '';
+      }
+    });
+    return locationMock;
+  };
+};
