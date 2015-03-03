6ed739f23158459034402cc13f07318b4d2f0318
Éric Lemoine <eric.lemoine@gmail.com>
2015-03-03 15:41:29 +0100

    Add language selector to measure example

--- examples/measure.js
+++ examples/measure.js
@@ -35,7 +35,8 @@ app.measuretoolsDirective = function() {
   return {
     restrict: 'A',
     scope: {
-      'map': '=appMeasuretoolsMap'
+      'map': '=appMeasuretoolsMap',
+      'lang': '=appMeasuretoolsLang'
     },
     controller: 'AppMeasuretoolsController',
     controllerAs: 'ctrl',
@@ -49,12 +50,63 @@ app.module.directive('appMeasuretools', app.measuretoolsDirective);
 
 
 /**
+ * @param {angular.Scope} $scope Angular scope.
+ * @param {angular.$compile} $compile Angular compile service.
+ * @param {angular.$sce} $sce Angular sce service.
  * @param {ngeo.DecorateInteraction} ngeoDecorateInteraction Decorate
  *     interaction service.
  * @constructor
  * @ngInject
  */
-app.MeasuretoolsController = function(ngeoDecorateInteraction) {
+app.MeasuretoolsController = function($scope, $compile, $sce,
+    ngeoDecorateInteraction) {
+
+  // Translations for the measure tools' tooltips.
+  var measureStartMsgs = {
+    'en': $sce.trustAsHtml('Click to start drawing.'),
+    'fr': $sce.trustAsHtml('Cliquer pour commencer à dessiner.')
+  };
+  var measureLengthContinueMsgs = {
+    'en': $sce.trustAsHtml('Click to continue drawing.<br>' +
+        'Double-click or click last point to finish.'),
+    'fr': $sce.trustAsHtml('Cliquer pour continuer le dessin.<br>' +
+        'Double-cliquer ou cliquer sur dernier point pour finir.')
+  };
+  var measureAreaContinueMsgs = {
+    'en': $sce.trustAsHtml('Click to continue drawing.<br>' +
+        'Double-click or click starting point to finish.'),
+    'fr': $sce.trustAsHtml('Cliquer pour continuer le dessin.<br>' +
+        'Double-cliquer ou cliquer sur point de départ pour finir.')
+  };
+  var measureAzimutContinueMsgs = {
+    'en': $sce.trustAsHtml('Click to finish.'),
+    'fr': $sce.trustAsHtml('Cliquer pour finir.')
+  };
+
+  // Create elements for the measure tools' tooltips.
+  var measureStartMsg = angular.element(
+      '<span ng-bind-html="ctrl.measureStartMsg"></span>');
+  measureStartMsg = $compile(measureStartMsg)($scope);
+  var measureLengthContinueMsg = angular.element(
+      '<span ng-bind-html="ctrl.measureLengthContinueMsg"></span>');
+  measureLengthContinueMsg = $compile(measureLengthContinueMsg)($scope);
+  var measureAreaContinueMsg = angular.element(
+      '<span ng-bind-html="ctrl.measureAreaContinueMsg"></span>');
+  measureAreaContinueMsg = $compile(measureAreaContinueMsg)($scope);
+  var measureAzimutContinueMsg = angular.element(
+      '<span ng-bind-html="ctrl.measureAzimutContinueMsg"></span>');
+  measureAzimutContinueMsg = $compile(measureAzimutContinueMsg)($scope);
+
+  // Watch the "lang" property and update the toolip messages
+  // based on the selected language.
+  $scope.$watch(angular.bind(this, function() {
+    return this['lang'];
+  }), angular.bind(this, function(newVal) {
+    this['measureStartMsg'] = measureStartMsgs[newVal];
+    this['measureLengthContinueMsg'] = measureLengthContinueMsgs[newVal];
+    this['measureAreaContinueMsg'] = measureAreaContinueMsgs[newVal];
+    this['measureAzimutContinueMsg'] = measureAzimutContinueMsgs[newVal];
+  }));
 
   var style = new ol.style.Style({
     fill: new ol.style.Fill({
@@ -80,7 +132,9 @@ app.MeasuretoolsController = function(ngeoDecorateInteraction) {
 
   /** @type {ngeo.interaction.MeasureLength} */
   var measureLength = new ngeo.interaction.MeasureLength({
-    sketchStyle: style
+    sketchStyle: style,
+    startMsg: measureStartMsg[0],
+    continueMsg: measureLengthContinueMsg[0]
   });
   measureLength.setActive(false);
   ngeoDecorateInteraction(measureLength);
@@ -89,7 +143,9 @@ app.MeasuretoolsController = function(ngeoDecorateInteraction) {
 
   /** @type {ngeo.interaction.MeasureArea} */
   var measureArea = new ngeo.interaction.MeasureArea({
-    sketchStyle: style
+    sketchStyle: style,
+    startMsg: measureStartMsg[0],
+    continueMsg: measureAreaContinueMsg[0]
   });
   measureArea.setActive(false);
   ngeoDecorateInteraction(measureArea);
@@ -98,7 +154,9 @@ app.MeasuretoolsController = function(ngeoDecorateInteraction) {
 
   /** @type {ngeo.interaction.MeasureAzimut} */
   var measureAzimut = new ngeo.interaction.MeasureAzimut({
-    sketchStyle: style
+    sketchStyle: style,
+    startMsg: measureStartMsg[0],
+    continueMsg: measureAzimutContinueMsg[0]
   });
   measureAzimut.setActive(false);
   ngeoDecorateInteraction(measureAzimut);
@@ -114,6 +172,9 @@ app.module.controller('AppMeasuretoolsController', app.MeasuretoolsController);
  * @constructor
  */
 app.MainController = function() {
+
+  /** @type {string} */
+  this['lang'] = 'en';
 
   /** @type {ol.Map} */
   var map = new ol.Map({
