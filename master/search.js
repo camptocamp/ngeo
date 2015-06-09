58fd8ffe6ccb968d01e42d3dddcbbd2a0df89970
Travis <travis@travis-ci.org>
2015-06-09 11:57:33 +0000

    Update GitHub pages

--- master/search.js
+++ master/search.js
@@ -153,7 +153,7 @@ app.SearchController.prototype.createAndInitBloodhound_ =
 
 
 /**
- * @param {jQuery.Event} event Event.
+ * @param {jQuery.event} event Event.
  * @param {Object} suggestion Suggestion.
  * @param {TypeaheadDataset} dataset Dataset.
  * @this {app.SearchController}
