35bd15a3420bf784eb571fa923faa877d53cdc1c
Frederic Junod <frederic.junod@camptocamp.com>
2015-11-23 12:18:48 +0100

    Use gmf.proj.EPSG21781

--- contribs/gmf/apps/mobile/js/app.js
+++ contribs/gmf/apps/mobile/js/app.js
@@ -1,6 +1,7 @@
 goog.provide('app.MainController');
 
 goog.require('app');
+goog.require('gmf.proj.EPSG21781');
 goog.require('ngeo.FeatureOverlayMgr');
 goog.require('ol.Map');
 goog.require('ol.View');
@@ -19,11 +20,6 @@ goog.require('ol.source.OSM');
  * @export
  */
 app.MainController = function(ngeoFeatureOverlayMgr) {
-
-  proj4.defs('EPSG:21781',
-      '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 ' +
-      '+x_0=600000 +y_0=200000 +ellps=bessel ' +
-      '+towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs');
 
   /**
    * @type {Array.<gmfx.SearchDirectiveDatasource>}
