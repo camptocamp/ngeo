2ddcf292e1611e702f2de6df5d88a8ee20c87c0c
Frederic Junod <frederic.junod@camptocamp.com>
2015-12-07 12:32:57 +0100

    Add new ngeo.recenterDirective example

---
+++ examples/locationchooser.js
@@ -0,0 +1,43 @@
+goog.provide('locationchooser');
+
+goog.require('ngeo.mapDirective');
+goog.require('ngeo.recenterDirective');
+goog.require('ol.Map');
+goog.require('ol.View');
+goog.require('ol.layer.Tile');
+goog.require('ol.source.OSM');
+
+
+/** @const **/
+var app = {};
+
+
+/** @type {!angular.Module} **/
+app.module = angular.module('app', ['ngeo']);
+
+
+
+/**
+ * @constructor
+ */
+app.MainController = function() {
+
+  /**
+   * @type {ol.Map}
+   * @export
+   */
+  this.map = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: [0, 0],
+      zoom: 4
+    })
+  });
+};
+
+
+app.module.controller('MainController', app.MainController);
