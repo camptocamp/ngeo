6257a7d60342ad1a52e52e7bb752225e1ff237de
Stéphane Brunner <stephane.brunner@camptocamp.com>
2015-12-09 15:39:22 +0100

    Fix jsdoc

--- contribs/gmf/apps/mobile/default.js
+++ contribs/gmf/apps/mobile/default.js
@@ -1,12 +1,12 @@
 /**
- * @type {string}
  * The default ngeo template based URL, used as it by the template cache.
+ * @type {string}
  */
 ngeo.baseTemplateUrl = '../../../../src/directives/partials';
 
 
 /**
- * @type {string}
  * The default gmf template based URL, used as it by the template cache.
+ * @type {string}
  */
 gmf.baseTemplateUrl = '../../src/directives/partials';
