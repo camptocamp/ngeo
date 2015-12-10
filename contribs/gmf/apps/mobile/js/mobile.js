59acd83fe6d768eeeeea3b76054e8bdc2f47edb3
Florent gravin <florent.gravin@camptocamp.com>
2015-12-10 14:26:34 +0100

    Add an AbstractMobileController in gmf

--- contribs/gmf/apps/mobile/js/mobile.js
+++ contribs/gmf/apps/mobile/js/mobile.js
@@ -11,21 +11,7 @@ goog.provide('app.MobileController');
 goog.provide('app_mobile');
 
 goog.require('app');
-goog.require('gmf.mapDirective');
-goog.require('gmf.mobileNavDirective');
-goog.require('gmf.proj.EPSG21781');
-goog.require('gmf.searchDirective');
-goog.require('ngeo.FeatureOverlayMgr');
-goog.require('ngeo.mobileGeolocationDirective');
-goog.require('ol.Map');
-goog.require('ol.View');
-goog.require('ol.control.ScaleLine');
-goog.require('ol.control.Zoom');
-goog.require('ol.layer.Tile');
-goog.require('ol.source.OSM');
-
-
-gmfModule.constant('isMobile', true);
+goog.require('gmf.AbstractMobileController');
 
 
 
@@ -34,137 +20,14 @@ gmfModule.constant('isMobile', true);
  *     overlay manager service.
  * @param {Object} serverVars vars from GMF
  * @constructor
+ * @extends {gmf.AbstractMobileController}
  * @ngInject
  * @export
  */
 app.MobileController = function(ngeoFeatureOverlayMgr, serverVars) {
-
-  /**
-   * @type {Array.<gmfx.SearchDirectiveDatasource>}
-   * @export
-   */
-  this.searchDatasources = [{
-    datasetTitle: 'Internal',
-    labelKey: 'label',
-    groupsKey: 'layer_name',
-    groupValues: ['osm'],
-    projection: 'EPSG:21781',
-    url: serverVars['serviceUrls']['fulltextsearch']
-  }];
-
-  /**
-   * @type {boolean}
-   * @export
-   */
-  this.leftNavVisible = false;
-
-  /**
-   * @type {boolean}
-   * @export
-   */
-  this.rightNavVisible = false;
-
-  var positionFeatureStyle = new ol.style.Style({
-    image: new ol.style.Circle({
-      radius: 6,
-      fill: new ol.style.Fill({color: 'rgba(230, 100, 100, 1)'}),
-      stroke: new ol.style.Stroke({color: 'rgba(230, 40, 40, 1)', width: 2})
-    })
-  });
-
-  var accuracyFeatureStyle = new ol.style.Style({
-    fill: new ol.style.Fill({color: 'rgba(100, 100, 230, 0.3)'}),
-    stroke: new ol.style.Stroke({color: 'rgba(40, 40, 230, 1)', width: 2})
-  });
-
-  /**
-   * @type {ngeox.MobileGeolocationDirectiveOptions}
-   * @export
-   */
-  this.mobileGeolocationOptions = {
-    positionFeatureStyle: positionFeatureStyle,
-    accuracyFeatureStyle: accuracyFeatureStyle,
-    zoom: 17
-  };
-
-  /**
-   * @type {ol.Map}
-   * @export
-   */
-  this.map = new ol.Map({
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: [0, 0],
-      zoom: 2
-    }),
-    controls: [
-      new ol.control.ScaleLine(),
-      new ol.control.Zoom()
-    ]
-  });
-
-  ngeoFeatureOverlayMgr.init(this.map);
-
+  goog.base(this, ngeoFeatureOverlayMgr, serverVars);
 };
-
-
-/**
- * @export
- */
-app.MobileController.prototype.toggleLeftNavVisibility = function() {
-  this.leftNavVisible = !this.leftNavVisible;
-};
-
-
-/**
- * @export
- */
-app.MobileController.prototype.toggleRightNavVisibility = function() {
-  this.rightNavVisible = !this.rightNavVisible;
-};
-
-
-/**
- * Hide both navigation menus.
- * @export
- */
-app.MobileController.prototype.hideNav = function() {
-  this.leftNavVisible = this.rightNavVisible = false;
-};
-
-
-/**
- * @return {boolean} Return true if one of the navigation menus is visible,
- * otherwise false.
- * @export
- */
-app.MobileController.prototype.navIsVisible = function() {
-  return this.leftNavVisible || this.rightNavVisible;
-};
-
-
-/**
- * @return {boolean} Return true if the left navigation menus is visible,
- * otherwise false.
- * @export
- */
-app.MobileController.prototype.leftNavIsVisible = function() {
-  return this.leftNavVisible;
-};
-
-
-/**
- * @return {boolean} Return true if the right navigation menus is visible,
- * otherwise false.
- * @export
- */
-app.MobileController.prototype.rightNavIsVisible = function() {
-  return this.rightNavVisible;
-};
+goog.inherits(app.MobileController, gmf.AbstractMobileController);
 
 
 appModule.controller('MobileController', app.MobileController);
