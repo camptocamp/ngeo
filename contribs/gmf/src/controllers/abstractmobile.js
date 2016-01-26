686e6cafb625b2c7da3dec924f53b50e8b1a5adf
Oliver Christen <oliver.christen@camptocamp.com>
2016-01-26 12:48:29 +0100

    Reduced zoom value when recentering. cf c2cgeoportal #1954.

--- contribs/gmf/src/controllers/abstractmobile.js
+++ contribs/gmf/src/controllers/abstractmobile.js
@@ -115,7 +115,7 @@ gmf.AbstractMobileController = function(
   this.mobileGeolocationOptions = {
     positionFeatureStyle: positionFeatureStyle,
     accuracyFeatureStyle: accuracyFeatureStyle,
-    zoom: 17
+    zoom: 9
   };
 
   /**
