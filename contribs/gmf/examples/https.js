1b97913d62c835b36c956472547734af2d4f27f0
Stéphane Brunner <stephane.brunner@camptocamp.com>
2016-02-02 16:56:15 +0100

    Don't forward to https for localhost

--- contribs/gmf/examples/https.js
+++ contribs/gmf/examples/https.js
@@ -2,7 +2,8 @@
  * Redirect to https
  */
 if (window.location.protocol == 'http:' &&
-    window.location.port != '3000') {
+    window.location.port != '3000' &&
+    window.location.host != 'localhost') {
   var restOfUrl = window.location.href.substr(5);
   /** @type {Location} */
   window.location = 'https:' + restOfUrl;
