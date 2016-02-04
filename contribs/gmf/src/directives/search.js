1f22444f45afc6c7387c3fb45b900c85f8c57561
Stefan Blattmann <stefan.blattmann@gmail.com>
2016-02-04 17:33:50 +0100

    Console.log removed

--- contribs/gmf/src/directives/search.js
+++ contribs/gmf/src/directives/search.js
@@ -198,8 +198,6 @@ gmf.SearchController = function($scope, $compile, $timeout, gettextCatalog,
         goog.isDef(datasource.groupsKey) ? datasource.groupValues : [];
     var filter;
 
-    console.log('groupValues: ', groupValues);
-
     do {
       var title = datasource.datasetTitle;
 
