42db741a77c67e2cb46035ff9a22aa2ba456a22d
Frederic Junod <frederic.junod@camptocamp.com>
2016-03-24 10:22:41 +0100

    Add new ngeo-zoom-delta atribute to ngeo.recenterDirective

--- src/directives/recenter.js
+++ src/directives/recenter.js
@@ -1,12 +1,14 @@
 goog.provide('ngeo.recenterDirective');
 
 goog.require('ngeo');
+goog.require('ol.animation');
+goog.require('ol.easing');
 
 
 /**
  * Provides the "ngeoRecenter" directive, a widget for recentering a map
- * to a specific extent (by using `ngeo-extent`) or a specific zoom level
- * (by using `ngeo-zoom`).
+ * to a specific extent (by using `ngeo-extent`), a specific zoom level
+ * (by using `ngeo-zoom`) or by a zoom delta (by using `ngeo-zoom-delta`).
  *
  * Example:
  *
@@ -14,6 +16,7 @@ goog.require('ngeo');
  *        <a href="#" ngeo-extent="[-1898084, 4676723, 3972279, 8590299]">A</a>
  *        <a href="#" ngeo-extent="[727681, 5784754, 1094579, 6029353]">B</a>
  *        <a href="#" ngeo-zoom="1">Zoom to level 1</a>
+ *        <a href="#" ngeo-zoom-delta="1">Zoom in</a>
  *      </div>
  *
  * Or with a select:
@@ -36,15 +39,28 @@ ngeo.recenterDirective = function() {
       var map = /** @type {ol.Map} */ ($scope.$eval(mapExpr));
 
       function recenter(element) {
+        var view = map.getView();
         var extent = element.attr('ngeo-extent');
         if (extent !== undefined) {
           var mapSize = /** @type {ol.Size} */ (map.getSize());
-          map.getView().fit($scope.$eval(extent), mapSize);
+          view.fit($scope.$eval(extent), mapSize);
         }
         var zoom = element.attr('ngeo-zoom');
         if (zoom !== undefined) {
-          map.getView().setZoom($scope.$eval(zoom));
+          view.setZoom($scope.$eval(zoom));
         }
+        var delta = element.attr('ngeo-zoom-delta');
+        if (delta !== undefined) {
+          var resolution = /** @type {number} */ (view.getResolution());
+          map.beforeRender(ol.animation.zoom({
+            resolution: resolution,
+            duration: 250,
+            easing: ol.easing.easeOut
+          }));
+          var newResolution = view.constrainResolution(resolution, $scope.$eval(delta));
+          view.setResolution(newResolution);
+        }
+
       }
 
       // if the children is a link or button
