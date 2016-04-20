977704ecc8fd412d0a9418d484e99a2fe9f97594
olive <olivier.semet@gmail.com>
2016-04-20 11:12:31 +0200

    UNITEST showing the bug described in #1029

--- contribs/gmf/test/spec/services/permalinkservice.spec.js
+++ contribs/gmf/test/spec/services/permalinkservice.spec.js
@@ -1,21 +1,22 @@
-/*global describe beforeEach inject  it expect themes */
+/*global describe beforeEach inject  it expect themes spyOn */
 /*eslint no-undef: "error"*/
 goog.require('gmf.Permalink');
 goog.require('gmf');
 goog.require('ngeo.LayerHelper');
+goog.require('ngeo.StateManager');
 goog.require('ol.Map');
 goog.require('ol.Collection');
 goog.require('ol.layer.Group');
 
 
 describe('Permalink service', function() {
-  var PermalinkService, map, LayerHelper, firstLevelGroup, secondLevelGroup, dataGroup;
+  var PermalinkService, map, LayerHelper, firstLevelGroup, secondLevelGroup, dataGroup, StateManagerService, osmThemeNode;
   var $injector;
 
   beforeEach(inject(function(_$injector_) {
 
     $injector = _$injector_;
-
+    StateManagerService = $injector.get('ngeoStateManager');
     PermalinkService = $injector.get('gmfPermalink');
     map = new ol.Map({layers : []});
     PermalinkService.setMap(map);
@@ -38,6 +39,10 @@ describe('Permalink service', function() {
 
     firstLevelGroup.getLayers().insertAt(0, secondLevelGroup);
     dataGroup.getLayers().insertAt(0, firstLevelGroup);
+
+    osmThemeNode = themes['themes'].filter(function(theme) {
+      return theme.name === 'OSM';
+    })[0];
 
   }));
 
@@ -74,6 +79,35 @@ describe('Permalink service', function() {
       }
     }
 
-  })
+  });
+
+  it('Should register a layer with the previous state saved in loacalStorage. TESTING MIXED GROUP ONLY', function() {
+
+    //delete non-mixed group from children
+    osmThemeNode.children = osmThemeNode.children.filter(function(node) {
+      return node.name === 'OSM function';
+    });
+
+    //only 1 theme for testing
+    PermalinkService.themes_ = [osmThemeNode];
+
+    //adding layer for each node to the map
+    var fakeLayers = osmThemeNode.children[0].children.map(function(node) {
+      var layer = new ol.layer.Layer({
+        layerName : node.name,
+        visible : true
+      });
+      dataGroup.getLayers().insertAt(0, layer);
+      return layer;
+    });
+
+    //mocking getInitialValue to return always false (and not use localStorage)
+    spyOn(StateManagerService, 'getInitialValue').and.returnValue(false);
+    PermalinkService.registerDataLayerGroup_(map);
+    expect(StateManagerService.getInitialValue).toHaveBeenCalled();
+    fakeLayers.forEach(function(layer) {
+      expect(layer.getVisible()).toBeFalsy();
+    });
+  });
 
 });
