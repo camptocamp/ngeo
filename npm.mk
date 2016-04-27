1fc4c0cbafa9a3956c49be1d340cf9164baa1912
Stéphane Brunner <stephane.brunner@camptocamp.com>
2016-04-27 11:17:38 +0200

    Remove no more needed path finder

--- npm.mk
+++ npm.mk
@@ -2,15 +2,11 @@
 .PHONY: install
 install: $(addprefix contribs/gmf/fonts/gmf-icons.,ttf eot woff)
 
-SVG2TTF_BIN = $(shell if [ -e node_modules/.bin/svg2ttf ]; then echo node_modules/.bin/svg2ttf; else echo ../.bin/svg2ttf; fi)
-TTF2EOT_BIN = $(shell if [ -e node_modules/.bin/ttf2eot ]; then echo node_modules/.bin/ttf2eot; else echo ../.bin/ttf2eot; fi)
-TTF2WOFF_BIN = $(shell if [ -e node_modules/.bin/ttf2woff ]; then echo node_modules/.bin/ttf2woff; else echo ../.bin/ttf2woff; fi)
-
 contribs/gmf/fonts/gmf-icons.ttf: contribs/gmf/fonts/gmf-icons.svg
-	$(SVG2TTF_BIN) $< $@
+	svg2ttf $< $@
 
 contribs/gmf/fonts/gmf-icons.eot: contribs/gmf/fonts/gmf-icons.ttf
-	$(TTF2EOT_BIN) $< $@
+	ttf2eot $< $@
 
 contribs/gmf/fonts/gmf-icons.woff: contribs/gmf/fonts/gmf-icons.ttf
-	$(TTF2WOFF_BIN) $< $@
+	ttf2woff $< $@
