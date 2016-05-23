5124ce2558e0642cf4d9844a81c51a32c57427fd
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2016-05-23 17:18:58 +0200

    Add filters for coordinates

--- src/services/filters.js
+++ src/services/filters.js
@@ -1,6 +1,7 @@
 goog.provide('ngeo.filters');
 
 goog.require('ngeo');
+goog.require('ol.coordinate');
 
 /**
  * Format a number as a localized scale.
@@ -21,11 +22,164 @@ goog.require('ngeo');
  * @ngname ngeoScalify
  */
 ngeo.Scalify = function($filter) {
-  var number = $filter('number');
+  var numberFilter = $filter('number');
   return function(scale) {
-    var text = number(scale, 0);
+    var text = numberFilter(scale, 0);
     return text ? '1\u00a0:\u00a0' + text : '';
   };
 };
 
 ngeo.module.filter('ngeoScalify', ngeo.Scalify);
+
+
+/**
+ * Format a couple of numbers as Swiss coordinates.
+ *
+ * Example without parameters:
+ *
+ *      <p>{{[600000.1234, 200000.5678] | ngeoSwissCoordinates}}</p>
+ *      <!-- will Become 600'000, 200'001 -->
+ *      <p>{{[2600000.1234, 1200000.5678] | ngeoSwissCoordinates}}</p>
+ *      <!-- will Become 2'600'000, 1'200'001 -->
+ *
+ * Example with defined prefix, separator and suffix (you can
+ *     not use the colon symbole).
+ *
+ *      <p>{{[600000.1234, 200000.5678] | ngeoSwissCoordinates:[:; :]}}</p>
+ *      <!-- will Become [600'000; 200'001] -->
+ *      <p>{{[2600000.1234, 1200000.5678] | ngeoSwissCoordinates:[:; :]}}</p>
+ *      <!-- will Become [2'600'000; 1'200'001] -->
+ *
+ * @return {function(ol.Coordinate, string, string, string): string} A
+ *     function to format numbers into an Swiss coordinates string.
+ * @ngInject
+ * @ngdoc filter
+ * @ngname ngeoSwissCoordinates
+ */
+ngeo.SwissCoordinates = function() {
+  /**
+   * @param {ol.Coordinate} coordinates Array of two numbers.
+   * @param {string=} opt_prefix Optional prefix.
+   * @param {string=} opt_separator Optional separatator. default to ' '.
+   * @param {string=} opt_suffix Optional suffix.
+   * @return {string} Swiss formated coordinates.
+   */
+  var filterFn = function(coordinates, opt_prefix, opt_separator, opt_suffix) {
+    var prefix = opt_prefix ? opt_prefix : '';
+    var separator = opt_separator ? opt_separator : ', ';
+    var suffix = opt_suffix ? opt_suffix : '';
+    var x = Math.round(coordinates[0]);
+    x = x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '\'');
+    var y = Math.round(coordinates[1]);
+    y = y.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '\'');
+    return prefix + x + separator + y + suffix;
+  };
+  return filterFn;
+};
+
+ngeo.module.filter('ngeoSwissCoordinates', ngeo.SwissCoordinates);
+
+
+/**
+ * Format a couple of numbers as a East North coordinates.
+ *
+ * Example without parameters:
+ *
+ *      <p>{{[7.1234, 46.9876] | ngeoEastNorthCoordinates}}</p>
+ *      <!-- will Become 7 E 47 N -->
+ *
+ * Example with defined fractionDigits, prefix, separator and suffix (you can
+ *     not use the colon symbole).
+ *
+ *      <p>{{[7.1234, 46.9876] | ngeoEastNorthCoordinates:2:[:; :]}}</p>
+ *      <!-- will Become [7.12 E; 46.99 N] -->
+ *
+ * @return {function(ol.Coordinate, number, string, string, string): string} A
+ *     function to format numbers into an East North coordinates string.
+ * @ngInject
+ * @ngdoc filter
+ * @ngname ngeoEastNorthCoordinates
+ */
+ngeo.EastNorthCoordinates = function() {
+  /**
+   * @param {ol.Coordinate} coordinates Array of two numbers.
+   * @param {number=} opt_fractionDigits number of digit, Same as
+   *     {@link ol.coordinate.format} opt_fractionDigits parameters.
+   * @param {string=} opt_prefix Optional prefix.
+   * @param {string=} opt_separator Optional separatator. default to ' '.
+   * @param {string=} opt_suffix Optional suffix.
+   * @return {string} East north formated coordinates.
+   */
+  var filterFn = function(coordinates, opt_fractionDigits, opt_prefix,
+      opt_separator, opt_suffix) {
+    var prefix = opt_prefix ? opt_prefix : '';
+    var separator = opt_separator ? opt_separator : ' ';
+    var suffix = opt_suffix ? opt_suffix : '';
+    var template = prefix + '{x} E' + separator + '{y} N' + suffix;
+    return ol.coordinate.format(coordinates, template, opt_fractionDigits);
+  };
+  return filterFn;
+};
+
+ngeo.module.filter('ngeoEastNorthCoordinates', ngeo.EastNorthCoordinates);
+
+
+/**
+ * Format a couple of numbers as DMS coordinates.
+ *
+ * Example without parameters:
+ *
+ *      <p>{{[7.1234, 46.9876] | ngeoDMSCoordinates}}</p>
+ *      <!-- will Become 46° 59' 15'' N 7° 07' 24'' E-->
+ *
+ * Example with defined fractionDigits, prefix, separator and suffix (you can
+ *     not use the colon symbole).
+ *
+ *      <p>{{[7.1234, 46.9876] | ngeoDMSCoordinates:2:[:; :]}}</p>
+ *      <!-- will Become [46° 59' 15.36'' N; 7° 07' 24.24'' E] -->
+ *
+ * @return {function(ol.Coordinate, number, string, string, string): string} A
+ *     function to format numbers into a DMS coordinates string.
+ * @ngInject
+ * @ngdoc filter
+ * @ngname ngeoDMSCoordinates
+ */
+ngeo.DMSCoordinates = function() {
+  var degreesToStringHDMS = function(degrees, hemispheres, digits) {
+    var normalizedDegrees = goog.math.modulo(degrees + 180, 360) - 180;
+    var dms = Math.abs(3600 * normalizedDegrees);
+    var d = Math.floor(dms / 3600);
+    var m = Math.floor((dms / 60) % 60);
+    var s = parseFloat((dms % 60).toFixed(digits));
+    return d + '\u00b0 ' +
+        goog.string.padNumber(m, 2) + '\u2032 ' +
+        goog.string.padNumber(s, 2) + '\u2033 ' +
+        hemispheres.charAt(normalizedDegrees < 0 ? 1 : 0);
+  };
+
+  /**
+   * @param {ol.Coordinate} coordinates Array of two numbers.
+   * @param {number=} opt_fractionDigits number of digit, Same as
+   *     {@link ol.coordinate.format} opt_fractionDigits parameters.
+   * @param {string=} opt_prefix Optional prefix.
+   * @param {string=} opt_separator Optional separatator. default to ' '.
+   * @param {string=} opt_suffix Optional suffix.
+   * @return {string} DMS formated coordinates.
+   */
+  var filterFn = function(coordinates, opt_fractionDigits, opt_prefix,
+      opt_separator, opt_suffix) {
+    var digits = opt_fractionDigits ? opt_fractionDigits : 0;
+    var prefix = opt_prefix ? opt_prefix : '';
+    var separator = opt_separator ? opt_separator : ' ';
+    var suffix = opt_suffix ? opt_suffix : '';
+
+    var xdms = degreesToStringHDMS(coordinates[1], 'NS', digits);
+    var ydms = degreesToStringHDMS(coordinates[0], 'EW', digits);
+
+    return prefix + xdms + separator + ydms + suffix;
+  };
+
+  return filterFn;
+};
+
+ngeo.module.filter('ngeoDMSCoordinates', ngeo.DMSCoordinates);
--- test/spec/services/filters.spec.js
+++ test/spec/services/filters.spec.js
@@ -8,8 +8,37 @@ describe('ngeo.Filters', function() {
     $filter = _$filter_;
   }));
 
-  it('ngeo scalify', function() {
+  it('Ngeo Scalify', function() {
       var ngeoScalify = $filter('ngeoScalify')
       expect(ngeoScalify(25000)).toBe('1\u00a0:\u00a025,000');
+  });
+
+  it('Ngeo Swiss coordinates', function() {
+      var ngeoSwissCoordinates = $filter('ngeoSwissCoordinates')
+      var co1 = [2600000.1234, 1200000.5678];
+      var co2 = [600000.1234, 200000.5678];
+      expect(ngeoSwissCoordinates(co1)).toBe('2\'600\'000, 1\'200\'001');
+      expect(ngeoSwissCoordinates(co2)).toBe('600\'000, 200\'001');
+      expect(ngeoSwissCoordinates(co1, '[', '; ', ']')).toBe(
+          '[2\'600\'000; 1\'200\'001]');
+      expect(ngeoSwissCoordinates(co2, '[', '; ', ']')).toBe(
+          '[600\'000; 200\'001]');
+  });
+
+  it('Ngeo East North coordinates', function() {
+      var ngeoEastNorthCoordinates = $filter('ngeoEastNorthCoordinates')
+      var co = [7.1234, 46.9876];
+      expect(ngeoEastNorthCoordinates(co)).toBe('7 E 47 N');
+      expect(ngeoEastNorthCoordinates(co, 2, '[', '; ', ']')).toBe(
+          '[7.12 E; 46.99 N]');
+  });
+
+  it('Ngeo DMS coordinates', function() {
+      var ngeoDMSCoordinates = $filter('ngeoDMSCoordinates')
+      var co = [7.1234, 46.9876];
+      expect(ngeoDMSCoordinates(co)).toBe(
+          '46\u00b0 59\u2032 15\u2033 N 7\u00b0 07\u2032 24\u2033 E');
+      expect(ngeoDMSCoordinates(co, 2, '[', '; ', ']')).toBe(
+          '[46\u00b0 59\u2032 15.36\u2033 N; 7\u00b0 07\u2032 24.24\u2033 E]');
   });
 });
