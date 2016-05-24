ed02de6256f9aa9a48d69bc597f1f27d4f8fa62d
Stéphane Brunner <stephane.brunner@camptocamp.com>
2016-05-24 11:04:12 +0200

    Add grab cursors for ie

--- npm.mk
+++ npm.mk
@@ -1,6 +1,7 @@
 
 .PHONY: install
-install: $(addprefix contribs/gmf/fonts/gmf-icons.,ttf eot woff)
+install: $(addprefix contribs/gmf/fonts/gmf-icons.,ttf eot woff) \
+	$(addprefix contribs/gmf/cursors/,grab.cur grabbing.cur)
 
 contribs/gmf/fonts/gmf-icons.ttf: contribs/gmf/fonts/gmf-icons.svg
 	svg2ttf $< $@
@@ -10,3 +11,6 @@ contribs/gmf/fonts/gmf-icons.eot: contribs/gmf/fonts/gmf-icons.ttf
 
 contribs/gmf/fonts/gmf-icons.woff: contribs/gmf/fonts/gmf-icons.ttf
 	ttf2woff $< $@
+
+contribs/gmf/cursors/%.cur: contribs/gmf/cursors/%.png
+	convert $< $@
