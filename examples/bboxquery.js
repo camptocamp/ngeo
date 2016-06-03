1efd2600b544b91565d33a1496ba7fcdecf3ade2
tsauerwein <tobias.sauerwein@camptocamp.com>
2016-06-03 17:21:23 +0200

    Add bbox query directive

---
+++ examples/bboxquery.js
@@ -0,0 +1,136 @@
+goog.provide('bboxquery');
+
+goog.require('ngeo.proj.EPSG21781');
+goog.require('ngeo');
+goog.require('ngeo.Query');
+goog.require('ngeo.btnDirective');
+goog.require('ngeo.mapDirective');
+goog.require('ngeo.bboxQueryDirective');
+goog.require('ol.Map');
+goog.require('ol.View');
+goog.require('ol.layer.Image');
+goog.require('ol.layer.Tile');
+goog.require('ol.proj');
+goog.require('ol.source.ImageWMS');
+goog.require('ol.source.OSM');
+
+
+/** @const **/
+var app = {};
+
+
+/** @type {!angular.Module} **/
+app.module = angular.module('app', ['ngeo']);
+
+
+app.module.constant('ngeoQueryOptions', {
+  'limit': 20
+});
+
+
+/**
+ * A sample directive to display the result.
+ *
+ * @return {angular.Directive} The directive specs.
+ */
+app.queryresultDirective = function() {
+  return {
+    restrict: 'E',
+    scope: {},
+    controller: 'AppQueryresultController',
+    controllerAs: 'qrCtrl',
+    bindToController: true,
+    templateUrl: 'partials/queryresult.html'
+  };
+};
+
+app.module.directive('appQueryresult', app.queryresultDirective);
+
+
+/**
+ * @param {ngeox.QueryResult} ngeoQueryResult The ngeo query service.
+ * @constructor
+ * @ngInject
+ */
+app.QueryresultController = function(ngeoQueryResult) {
+
+  /**
+   * @type {ngeox.QueryResult}
+   * @export
+   */
+  this.result = ngeoQueryResult;
+
+};
+
+
+app.module.controller('AppQueryresultController', app.QueryresultController);
+
+
+/**
+ * @param {angular.Scope} $scope Scope.
+ * @param {ngeo.Query} ngeoQuery The ngeo query service
+ * @constructor
+ * @ngInject
+ */
+app.MainController = function($scope, ngeoQuery) {
+
+  /**
+   * @type {boolean}
+   * @export
+   */
+  this.queryActive = true;
+
+  var projection = ol.proj.get('EPSG:21781');
+  projection.setExtent([485869.5728, 76443.1884, 837076.5648, 299941.7864]);
+
+  var busStopSourceId = 'bus_stop';
+  var busStopLayer = new ol.layer.Image({
+    'querySourceId': busStopSourceId,
+    'source': new ol.source.ImageWMS({
+      'url': 'https://geomapfish-demo.camptocamp.net/1.6/wsgi/mapserv_proxy',
+      params: {'LAYERS': 'bus_stop'}
+    })
+  });
+  ngeoQuery.addSource({
+    'id': busStopSourceId,
+    'layer': busStopLayer,
+    'wfsQuery': true // urlWfs?
+  });
+
+  var informationSourceId = 'information';
+  var informationLayer = new ol.layer.Image({
+    'querySourceId': informationSourceId,
+    'source': new ol.source.ImageWMS({
+      'url': 'https://geomapfish-demo.camptocamp.net/1.6/wsgi/mapserv_proxy',
+      params: {'LAYERS': 'information'}
+    })
+  });
+  ngeoQuery.addSource({
+    'id': informationSourceId,
+    'layer': informationLayer,
+    'wfsQuery': true // urlWfs?
+  });
+
+  /**
+   * @type {ol.Map}
+   * @export
+   */
+  this.map = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      }),
+      informationLayer,
+      busStopLayer
+    ],
+    view: new ol.View({
+      projection: projection,
+      resolutions: [200, 100, 50, 20, 10, 5, 2.5, 2, 1, 0.5],
+      center: [537635, 152640],
+      zoom: 0
+    })
+  });
+
+};
+
+app.module.controller('MainController', app.MainController);
---
+++ src/directives/bboxquery.js
@@ -0,0 +1,77 @@
+goog.provide('ngeo.bboxQueryDirective');
+
+goog.require('ngeo');
+goog.require('ngeo.Query');
+goog.require('ol.interaction.DragBox');
+
+
+/**
+ * Provides a "bbox query" directive.
+ *
+ * This directive is responsible of binding a map and the ngeo query service
+ * together. While active, drawing a bbox while CTRL or the 'meta' key is pressed
+ * issues a request to the query service.
+ *
+ * This directive doesn't require to be rendered in a visible DOM element, but
+ * it could be used with a ngeo-btn to manage the activation of the directive.
+ * See below an example without any use of UI:
+ *
+ * Example:
+ *
+ *      <span
+ *        ngeo-bbox-query=""
+ *        ngeo-bbox-query-map="::ctrl.map"
+ *        ngeo-bbox-query-active="ctrl.queryActive">
+ *      </span>
+ *
+ * See the live example: {@link ../examples/bboxquery.html}
+ *
+ * @param {ngeo.Query} ngeoQuery The ngeo Query service.
+ * @return {angular.Directive} The Directive Definition Object.
+ * @ngInject
+ * @ngdoc directive
+ * @ngname ngeoBboxQuery
+ */
+ngeo.bboxQueryDirective = function(ngeoQuery) {
+  return {
+    restrict: 'A',
+    scope: false,
+    link: function(scope, elem, attrs) {
+      /**
+       * @type {ol.Map}
+       */
+      var map = scope.$eval(attrs['ngeoBboxQueryMap']);
+
+      var interaction = new ol.interaction.DragBox({
+        condition: ol.events.condition.platformModifierKeyOnly
+      });
+
+      /**
+       * Called when a bbox is drawn while this controller is active. Issue
+       * a request to the query service using the extent that was drawn.
+       * @param {ol.DragBoxEvent} evt Event.
+       */
+      var handleBoxEnd = function(evt) {
+        var extent = interaction.getGeometry().getExtent();
+        ngeoQuery.issue(map, extent);
+      };
+      interaction.on('boxend', handleBoxEnd);
+
+      // watch 'active' property -> activate/deactivate accordingly
+      scope.$watch(attrs['ngeoBboxQueryActive'],
+          function(newVal, oldVal) {
+            if (newVal) {
+              // activate
+              map.addInteraction(interaction);
+            } else {
+              // deactivate
+              map.removeInteraction(interaction);
+              ngeoQuery.clear();
+            }
+          }
+      );
+    }
+  };
+};
+
+ngeo.module.directive('ngeoBboxQuery', ngeo.bboxQueryDirective);
