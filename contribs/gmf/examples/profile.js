de6712830e43f187617cb1a67af9131bbd160460
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2016-06-13 11:07:50 +0200

    Create GMF profile components and related example.

---
+++ contribs/gmf/examples/profile.js
@@ -0,0 +1,133 @@
+goog.provide('gmf-profile');
+
+goog.require('ngeo.FeatureOverlayMgr');
+goog.require('gmf.mapDirective');
+goog.require('gmf.profileDirective');
+goog.require('ngeo.proj.EPSG21781');
+goog.require('ol.Map');
+goog.require('ol.View');
+goog.require('ol.geom.Point');
+goog.require('ol.interaction.Draw');
+goog.require('ol.layer.Tile');
+goog.require('ol.proj');
+goog.require('ol.source.OSM');
+goog.require('ol.style.Circle');
+goog.require('ol.style.Fill');
+goog.require('ol.style.Stroke');
+goog.require('ol.style.Style');
+
+
+/** @const **/
+var app = {};
+
+
+/** @type {!angular.Module} **/
+app.module = angular.module('app', ['gmf']);
+
+
+app.module.constant(
+    'gmfProfileUrl',
+    'https://geomapfish-demo.camptocamp.net/2.1/wsgi/profile.json');
+
+
+/**
+ * @param {angular.Scope} $scope Angular scope.
+ * @param {ngeo.FeatureOverlayMgr} ngeoFeatureOverlayMgr Feature overlay
+ *     manager.
+ * @constructor
+ * @ngInject
+ */
+app.MainController = function($scope, ngeoFeatureOverlayMgr) {
+
+  var projection = ol.proj.get('EPSG:21781');
+
+  /**
+   * @type {ol.Map}
+   * @export
+   */
+  this.map = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      projection: projection,
+      resolutions: [200, 100, 50, 20, 10, 5, 2.5, 2, 1, 0.5],
+      center: [600000, 200000],
+      zoom: 3
+    })
+  });
+
+  /**
+   * @type {ol.Collection.<ol.Feature>}
+   * @export
+   */
+  var features = new ol.Collection();
+
+  var overlay = ngeoFeatureOverlayMgr.getFeatureOverlay();
+  overlay.setFeatures(features);
+  overlay.setStyle(new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: 'rgba(255, 255, 255, 0.2)'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#ffcc33',
+      width: 2
+    }),
+    image: new ol.style.Circle({
+      radius: 7,
+      fill: new ol.style.Fill({
+        color: '#ffcc33'
+      })
+    })
+  }));
+
+  // initialize the feature overlay manager with the map
+  ngeoFeatureOverlayMgr.init(this.map);
+
+  /**
+   * @type {ol.geom.LineString}
+   * @export
+   */
+  this.profileLine = null;
+
+  /**
+   * @type {ol.interaction.Draw}
+   * @export
+   */
+  this.drawLine = new ol.interaction.Draw(
+      /** @type {olx.interaction.DrawOptions} */ ({
+        type: 'LineString',
+        features: features
+      }));
+
+  this.drawLine.setActive(false);
+  this.map.addInteraction(this.drawLine);
+
+  this.clear_ = function() {
+    features.clear();
+    this.profileLine = null;
+  };
+
+  this.toggleDrawLineActive = function() {
+    if (this.drawLine.getActive()) {
+      this.drawLine.setActive(false);
+      this.clear_();
+    } else {
+      this.drawLine.setActive(true);
+    }
+  };
+
+  this.drawLine.on('drawstart', function() {
+    this.clear_();
+  }, this);
+
+  this.drawLine.on('drawend', function(e) {
+    this.profileLine = e.feature.getGeometry();
+    $scope.$digest();
+  }, this);
+};
+
+
+app.module.controller('MainController', app.MainController);
---
+++ contribs/gmf/src/directives/profile.js
@@ -0,0 +1,224 @@
+goog.provide('gmf.ProfileController');
+goog.provide('gmf.profileDirective');
+
+goog.require('gmf');
+goog.require('ngeo.FeatureOverlay');
+goog.require('ngeo.FeatureOverlayMgr');
+/** @suppress {extraRequire} */
+goog.require('ngeo.profileDirective');
+goog.require('ol.Feature');
+goog.require('ol.style.Circle');
+goog.require('ol.style.Fill');
+goog.require('ol.style.Style');
+
+
+ngeo.module.value('gmfProfileTemplateUrl',
+    /**
+     * @param {angular.JQLite} element Element.
+     * @param {angular.Attributes} attrs Attributes.
+     * @return {string} Template.
+     */
+    function(element, attrs) {
+      var templateUrl = attrs['gmfProfileTemplateurl'];
+      return templateUrl !== undefined ? templateUrl :
+          gmf.baseTemplateUrl + '/profile.html';
+    });
+
+
+/**
+ * @htmlAttribute {ol.Map} gmf-profile-map TODO
+ * @htmlAttribute {ol.Collection.<ol.Feature>} gmf-profile-features TODO
+ * @param {string} gmfProfileTemplateUrl URL to a template.
+ * @return {angular.Directive} Directive Definition Object.
+ * @ngInject
+ * @ngdoc directive
+ * @ngname gmfProfile
+ */
+gmf.profileDirective = function(gmfProfileTemplateUrl) {
+  return {
+    bindToController: true,
+    controller: 'GmfProfileController',
+    controllerAs: 'ctrl',
+    templateUrl: gmfProfileTemplateUrl,
+    replace: true,
+    restrict: 'E',
+    scope: {
+      'getMapFn': '&gmfProfileMap',
+      'line': '<gmfProfileLine'
+    }
+  };
+};
+
+
+gmf.module.directive('gmfProfile', gmf.profileDirective);
+
+
+/**
+ * @param {angular.Scope} $scope Angular scope.
+ * @param {angular.$http} $http Angular http service.
+ * @param {ngeo.FeatureOverlayMgr} ngeoFeatureOverlayMgr The ngeo feature
+ *     overlay manager service.
+ * @param {string} gmfProfileUrl URL of GMF service JSON profile.
+ * @constructor
+ * @export
+ * @ngInject
+ * @ngdoc Controller
+ * @ngname GmfProfileController
+ */
+gmf.ProfileController = function($scope, $http, ngeoFeatureOverlayMgr,
+    gmfProfileUrl) {
+
+  /**
+   * @type {angular.$http}
+   * @private
+   */
+  this.$http_ = $http;
+
+  /**
+   * @type {string}
+   * @private
+   */
+  this.gmfProfileUrl_ = gmfProfileUrl;
+
+  var map = this['getMapFn']();
+  goog.asserts.assertInstanceof(map, ol.Map);
+
+  /**
+   * @type {!ol.Map}
+   * @private
+   */
+  this.map_ = map;
+
+  /**
+   * @type {ol.geom.LineString}
+   * @export
+   */
+  this.line;
+
+  /**
+   * @type {Array.<Object>} FIXME
+   * @export
+   */
+  this.profileData = [];
+
+  /**
+   * @type {ngeo.FeatureOverlay}
+   * @private
+   */
+  this.featureOverlay_ = ngeoFeatureOverlayMgr.getFeatureOverlay();
+
+  this.featureOverlay_.setStyle(
+    new ol.style.Style({
+      image: new ol.style.Circle({
+        radius: 3,
+        fill: new ol.style.Fill({color: '#ffffff'})
+      })
+    }));
+
+  /**
+   * @type {ol.Feature}
+   * @private
+   */
+  this.snappedPoint_ = new ol.Feature();
+  this.featureOverlay_.addFeature(this.snappedPoint_);
+
+  var extractor = {
+    dist: this.getDist_,
+    z: this.getZ_
+  };
+
+  /**
+   * @type {*} FIXME
+   * @export
+   */
+  this.profileOptions = {
+    elevationExtractor: extractor
+  };
+
+  /**
+   * @type {boolean}
+   * @export
+   */
+  this.active = false;
+
+  $scope.$watch(
+    function() {
+      return this.line;
+    }.bind(this),
+    function(oldLine, newLine) {
+      if (oldLine !== newLine) {
+        this.update_();
+      }
+    }.bind(this));
+};
+
+
+/**
+ * @private
+ */
+gmf.ProfileController.prototype.update_ = function() {
+  if (this.line) {
+    this.getJsonProfile_();
+  } else {
+    this.profileData = [];
+  }
+};
+
+gmf.ProfileController.prototype.getJsonProfile_ = function() {
+//  var geom = {
+//    'type': 'LineString',
+//    'coordinates': this.line.getCoordinates()
+//  };
+//
+//  var data = {
+//    'layers': ['srtm', 'aster'],
+//    'geom': geom,
+//    'nbPoints': 100
+//  };
+//
+//  this.$http_.post(this.gmfProfileUrl_, form_data).then(
+//    this.getProfileDataSuccess_.bind(this),
+//    this.getProfileDataError_.bind(this)
+//  );
+  this.$http_.get('../../../../examples/data/profile.json').then(
+    this.getProfileDataSuccess_.bind(this),
+    this.getProfileDataError_.bind(this)
+  );
+};
+
+gmf.ProfileController.prototype.getProfileDataSuccess_ = function(resp) {
+  //this.profileData = resp.data;
+  this.profileData = resp.data.profile;
+};
+
+gmf.ProfileController.prototype.getProfileDataError_ = function(resp) {
+  console.error('Can not get JSON profile. ' + resp.statusText);
+};
+
+/**
+ * TODO
+ * @param {Object} item The item.
+ * @return {number} The elevation.
+ * @private
+ */
+gmf.ProfileController.prototype.getZ_ = function(item) {
+  if ('values' in item && 'mnt' in item['values']) {
+    return parseFloat((item['values']['mnt'] / 100).toPrecision(5));
+  }
+  return 0;
+};
+
+/**
+ * TODO
+ * @param {Object} item The item.
+ * @return {number} The distance.
+ * @private
+ */
+gmf.ProfileController.prototype.getDist_ = function(item) {
+  if ('dist' in item) {
+    return item['dist'];
+  }
+  return 0;
+};
+
+gmf.module.controller('GmfProfileController', gmf.ProfileController);
