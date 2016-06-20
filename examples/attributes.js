933d7a5c8f2f125f79b3022186ff52745c483b27
Alexandre Dubé <adube@mapgears.com>
2016-06-20 14:19:50 -0400

    Add XSDAttributes format

---
+++ examples/attributes.js
@@ -0,0 +1,39 @@
+goog.provide('attributes');
+
+goog.require('ngeo.mapDirective');
+goog.require('ngeo.format.XSDAttribute');
+
+
+/** @const **/
+var app = {};
+
+
+/** @type {!angular.Module} **/
+app.module = angular.module('app', ['ngeo']);
+
+
+/**
+ * @param {angular.$http} $http Angular http service.
+ * @constructor
+ */
+app.MainController = function($http) {
+
+  $http.get('data/xsdattributes.xml').then(
+    /**
+     * @param {angular.$http.Response} resp Ajax response.
+     * @return {Array.<ngeox.Attribute>} List of attributes.
+     * @private
+     */
+    function(resp) {
+      var format = new ngeo.format.XSDAttribute();
+      var attributes = format.read(resp.data);
+      document.getElementById('list-of-attributes').innerHTML =
+        JSON.stringify(attributes);
+      return resp;
+
+    }
+  );
+};
+
+
+app.module.controller('MainController', app.MainController);
