e66caf6b686e9c45883e1e712c5930315dbd436e
olive <olivier.semet@gmail.com>
2016-07-12 17:11:55 +0200

    fixing gmf print example

--- src/services/print.js
+++ src/services/print.js
@@ -1,11 +1,13 @@
 goog.provide('ngeo.CreatePrint');
 goog.provide('ngeo.Print');
 
+
 goog.require('goog.color');
 goog.require('goog.color.alpha');
 goog.require('goog.math');
 goog.require('goog.object');
 goog.require('ngeo');
+goog.require('ngeo.LayerHelper');
 goog.require('ol.color');
 goog.require('ol.format.GeoJSON');
 goog.require('ol.geom.GeometryType');
