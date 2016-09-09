e3a6ad460102758130eeb1805a6b737243a9d1c9
Alexandre Dubé <adube@mapgears.com>
2016-09-09 08:29:37 -0400

    Fix Travis issue

--- contribs/gmf/src/directives/print.js
+++ contribs/gmf/src/directives/print.js
@@ -2,6 +2,7 @@ goog.provide('gmf.PrintController');
 goog.provide('gmf.printDirective');
 
 goog.require('gmf');
+/** @suppress {extraRequire} */
 goog.require('gmf.authenticationDirective');
 goog.require('ngeo.CreatePrint');
 goog.require('ngeo.FeatureOverlayMgr');
