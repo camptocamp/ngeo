959e36b0cb23cf620ec64defddce23f2fb401eeb
Alexandre Dubé <adube@mapgears.com>
2016-09-19 15:24:26 -0400

    Fix editfeature and selector directives for LayerTree refactoring

--- contribs/gmf/examples/editfeatureselector.js
+++ contribs/gmf/examples/editfeatureselector.js
@@ -2,11 +2,12 @@ goog.provide('gmf-editfeatureselector');
 
 goog.require('gmf');
 goog.require('gmf.Themes');
+goog.require('gmf.TreeManager');
 goog.require('gmf.authenticationDirective');
 goog.require('gmf.editfeatureselectorDirective');
+goog.require('gmf.layertreeDirective');
 goog.require('gmf.mapDirective');
 goog.require('ngeo.FeatureHelper');
-goog.require('ngeo.LayerHelper');
 goog.require('ngeo.ToolActivate');
 goog.require('ngeo.ToolActivateMgr');
 goog.require('ngeo.proj.EPSG21781');
@@ -26,6 +27,14 @@ var app = {};
 /** @type {!angular.Module} **/
 app.module = angular.module('app', ['gmf']);
 
+
+app.module.value('gmfTreeUrl',
+    'https://geomapfish-demo.camptocamp.net/2.1/wsgi/themes?version=2&background=background');
+
+
+app.module.value('gmfWmsUrl',
+    'https://geomapfish-demo.camptocamp.net/2.1/wsgi/mapserv_proxy');
+
 app.module.value(
     'authenticationBaseUrl',
     'https://geomapfish-demo.camptocamp.net/2.1/wsgi');
@@ -42,15 +51,15 @@ app.module.value('gmfLayersUrl',
 /**
  * @param {!angular.Scope} $scope Angular scope.
  * @param {gmf.Themes} gmfThemes The gmf themes service.
+ * @param {gmf.TreeManager} gmfTreeManager gmf Tree Manager service.
  * @param {gmfx.User} gmfUser User.
  * @param {ngeo.FeatureHelper} ngeoFeatureHelper Ngeo feature helper service.
- * @param {ngeo.LayerHelper} ngeoLayerHelper Ngeo Layer Helper.
  * @param {ngeo.ToolActivateMgr} ngeoToolActivateMgr Ngeo ToolActivate manager
  *     service.
  * @constructor
  */
-app.MainController = function($scope, gmfThemes, gmfUser, ngeoFeatureHelper,
-    ngeoLayerHelper, ngeoToolActivateMgr) {
+app.MainController = function($scope, gmfThemes, gmfTreeManager, gmfUser,
+    ngeoFeatureHelper, ngeoToolActivateMgr) {
 
   /**
    * @type {!angular.Scope}
@@ -72,20 +81,15 @@ app.MainController = function($scope, gmfThemes, gmfUser, ngeoFeatureHelper,
 
   gmfThemes.loadThemes();
 
+  /**
+   * @type {gmf.TreeManager}
+   * @export
+   */
+  this.gmfTreeManager = gmfTreeManager;
+
   var projection = ol.proj.get('EPSG:21781');
   projection.setExtent([485869.5728, 76443.1884, 837076.5648, 299941.7864]);
 
-  /**
-   * @type {ol.layer.Image}
-   * @private
-   */
-  var wmsLayer = new ol.layer.Image({
-    editableIds: [111, 112, 113],
-    source: new ol.source.ImageWMS({
-      url: 'https://geomapfish-demo.camptocamp.net/2.1/wsgi/mapserv_proxy',
-      params: {'LAYERS': 'point,line,polygon'}
-    })
-  });
 
   /**
    * @type {ol.layer.Vector}
@@ -100,6 +104,12 @@ app.MainController = function($scope, gmfThemes, gmfUser, ngeoFeatureHelper,
       return ngeoFeatureHelper.createEditingStyles(feature);
     }
   });
+
+  /**
+   * @type {Object|undefined}
+   * @export
+   */
+  this.treeSource = undefined;
 
   /**
    * @type {ol.Map}
@@ -119,14 +129,21 @@ app.MainController = function($scope, gmfThemes, gmfUser, ngeoFeatureHelper,
     })
   });
 
-  // Add the WMS layer to the 'data' group, which is what the layer tree
-  // would do
-  var dataLayerGroup = ngeoLayerHelper.getGroupFromMap(this.map,
-        gmf.DATALAYERGROUP_NAME);
-  dataLayerGroup.getLayers().push(wmsLayer);
+  gmfThemes.getThemesObject().then(function(themes) {
+    if (themes) {
+      // Add 'Edit' theme, i.e. the one with id 73
+      for (var i = 0, ii = themes.length; i < ii; i++) {
+        if (themes[i].id === 73) {
+          this.gmfTreeManager.addTheme(themes[i]);
+          break;
+        }
+      }
+      this.treeSource = this.gmfTreeManager.tree;
 
-  // Add layer vector after
-  this.map.addLayer(this.vectorLayer);
+      // Add layer vector after
+      this.map.addLayer(this.vectorLayer);
+    }
+  }.bind(this));
 
  /**
    * @type {boolean}
