bba9e2a1868c180434e7f9fe5323d8abc4f91a84
Alexandre Dubé <adube@mapgears.com>
2016-11-08 10:12:46 -0500

    #2594 oe refresh wms (wip)

--- contribs/gmf/examples/objectediting.js
+++ contribs/gmf/examples/objectediting.js
@@ -1,5 +1,9 @@
 goog.provide('gmf-objectediting');
 
+goog.require('gmf');
+goog.require('gmf.Themes');
+goog.require('gmf.TreeManager');
+goog.require('gmf.layertreeDirective');
 goog.require('gmf.mapDirective');
 goog.require('gmf.objecteditingDirective');
 goog.require('gmf.ObjectEditingManager');
@@ -30,14 +34,28 @@ app.module.value('gmfLayersUrl',
     'https://geomapfish-demo.camptocamp.net/2.1/wsgi/layers/');
 
 
+gmf.module.value('gmfTreeManagerModeFlush', true);
+
+
 /**
  * @param {gmf.ObjectEditingManager} gmfObjectEditingManager The gmf
  *     ObjectEditing manager service.
+ * @param {gmf.Themes} gmfThemes The gmf themes service.
+ * @param {gmf.TreeManager} gmfTreeManager gmf Tree Manager service.
  * @param {ngeo.ToolActivateMgr} ngeoToolActivateMgr Ngeo ToolActivate manager
  *     service.
  * @constructor
  */
-app.MainController = function(gmfObjectEditingManager, ngeoToolActivateMgr) {
+app.MainController = function(gmfObjectEditingManager, gmfThemes,
+    gmfTreeManager, ngeoToolActivateMgr) {
+
+  /**
+   * @type {gmf.TreeManager}
+   * @private
+   */
+  this.gmfTreeManager_ = gmfTreeManager;
+
+  gmfThemes.loadThemes();
 
   var projection = ol.proj.get('EPSG:21781');
   projection.setExtent([485869.5728, 76443.1884, 837076.5648, 299941.7864]);
@@ -50,7 +68,11 @@ app.MainController = function(gmfObjectEditingManager, ngeoToolActivateMgr) {
     wrapX: false
   });
 
-  var vectorLayer = new ol.layer.Vector({
+  /**
+   * @type {ol.layer.Vector}
+   * @private
+   */
+  this.vectorLayer_ = new ol.layer.Vector({
     source: this.vectorSource_
   });
 
@@ -62,8 +84,7 @@ app.MainController = function(gmfObjectEditingManager, ngeoToolActivateMgr) {
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM()
-      }),
-      vectorLayer
+      })
     ],
     view: new ol.View({
       projection: projection,
@@ -72,6 +93,21 @@ app.MainController = function(gmfObjectEditingManager, ngeoToolActivateMgr) {
       zoom: 2
     })
   });
+
+  gmfThemes.getThemesObject().then(function(themes) {
+    if (themes) {
+      // Add 'ObjectEditing' theme, i.e. the one with id 168
+      for (var i = 0, ii = themes.length; i < ii; i++) {
+        if (themes[i].id === 168) {
+          this.gmfTreeManager_.setFirstLevelGroups(themes[i].children);
+          break;
+        }
+      }
+
+      // Add layer vector after
+      this.map.addLayer(this.vectorLayer_);
+    }
+  }.bind(this));
 
   /**
    * @type {?number}
