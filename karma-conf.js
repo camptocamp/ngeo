0ac25d8ef7e921dc65dfb9008ead94a6a3c2f1e6
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2017-01-05 13:49:14 +0100

    With this, karma should run in Chromium.
    Then buildtools/check-example.js should be taken care of by launching chromium with:
    chromium-browser --enable-logging=stderr --log-level=2

--- karma-conf.js
+++ karma-conf.js
@@ -116,8 +116,14 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ['optchrome'],
 
+    customLaunchers: {
+      optchrome: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
