fe0acbb573f5041733bfdf8cbf261c67d7e73d0e
Travis <travis@travis-ci.org>
2017-02-03 15:59:19 +0000

    Cleanup GitHub pages

--- test/examples/default.js
+++
@@ -1,5 +0,0 @@
-/**
- * The default template based URL, used as it by the template cache.
- * @type {string}
- */
-ngeo.baseTemplateUrl = '../src/directives/partials';
--- test/examples/popover.js
+++
@@ -1,7 +0,0 @@
-
-
-/** @const **/
-var app = {};
-
-/** @type {!angular.Module} **/
-app.module = angular.module('app', ['ngeo']);
