d0266af2c27345a6c26fcd47051aec115aba58e7
Frederic Junod <frederic.junod@camptocamp.com>
2017-04-27 10:41:59 +0200

    Fix geolocation untrack test

--- src/directives/mobilegeolocation.js
+++ src/directives/mobilegeolocation.js
@@ -222,9 +222,10 @@ ngeo.MobileGeolocationController.prototype.toggleTracking = function() {
       return;
     }
     goog.asserts.assert(currentPosition !== undefined);
+    // stop tracking if the position is close to the center of the map.
     var center = this.map_.getView().getCenter();
-    if (currentPosition[0] === center[0] &&
-        currentPosition[1] === center[1]) {
+    var diff = Math.abs(currentPosition[0] - center[0]) + Math.abs(currentPosition[1] - center[1]);
+    if (diff < 2) {
       this.untrack_();
     } else {
       this.untrack_();
