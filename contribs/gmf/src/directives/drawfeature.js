a6b9a7a87f4760ccabaa846e9123fdebdb15b5a0
Laurent Lienher <laurent.lienher@camptocamp.com>
2017-07-19 08:32:53 +0200

    Add a tolerance for the feature selection click

--- contribs/gmf/src/directives/drawfeature.js
+++ contribs/gmf/src/directives/drawfeature.js
@@ -564,6 +564,9 @@ gmf.DrawfeatureController.prototype.handleMapClick_ = function(evt) {
         ret = feature;
       }
       return ret;
+    },
+    {
+      hitTolerance: 5
     }
   );
 
--- contribs/gmf/src/directives/editfeature.js
+++ contribs/gmf/src/directives/editfeature.js
@@ -874,6 +874,9 @@ gmf.EditfeatureController.prototype.handleMapClick_ = function(evt) {
         ret = feature;
       }
       return ret;
+    },
+    {
+      hitTolerance: 5
     }
   );
 
