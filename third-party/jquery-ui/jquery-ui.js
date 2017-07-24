aae51d591864267a714370cb015358ecba8d90aa
Roman Zoller <roman.zoller@camptocamp.com>
2017-07-24 14:44:31 +0200

    Add de/fr datepicker locales

--- third-party/jquery-ui/jquery-ui.js
+++ third-party/jquery-ui/jquery-ui.js
@@ -7269,3 +7269,81 @@ var widgetsSlider = $.widget( "ui.slider", $.ui.mouse, {
 
 
 }));
+
+/* German initialisation for the jQuery UI date picker plugin. */
+/* Written by Milian Wolff (mail@milianw.de). */
+( function( factory ) {
+  if ( typeof define === "function" && define.amd ) {
+
+    // AMD. Register as an anonymous module.
+    define( [ "../widgets/datepicker" ], factory );
+  } else {
+
+    // Browser globals
+    factory( jQuery.datepicker );
+  }
+}( function( datepicker ) {
+
+  datepicker.regional.de = {
+    closeText: "Schließen",
+    prevText: "&#x3C;Zurück",
+    nextText: "Vor&#x3E;",
+    currentText: "Heute",
+    monthNames: [ "Januar","Februar","März","April","Mai","Juni",
+      "Juli","August","September","Oktober","November","Dezember" ],
+    monthNamesShort: [ "Jan","Feb","Mär","Apr","Mai","Jun",
+      "Jul","Aug","Sep","Okt","Nov","Dez" ],
+    dayNames: [ "Sonntag","Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag" ],
+    dayNamesShort: [ "So","Mo","Di","Mi","Do","Fr","Sa" ],
+    dayNamesMin: [ "So","Mo","Di","Mi","Do","Fr","Sa" ],
+    weekHeader: "KW",
+    dateFormat: "dd.mm.yy",
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: "" };
+  datepicker.setDefaults( datepicker.regional.de );
+
+  return datepicker.regional.de;
+
+} ) );
+
+/* French initialisation for the jQuery UI date picker plugin. */
+/* Written by Keith Wood (kbwood{at}iinet.com.au),
+ Stéphane Nahmani (sholby@sholby.net),
+ Stéphane Raimbault <stephane.raimbault@gmail.com> */
+( function( factory ) {
+  if ( typeof define === "function" && define.amd ) {
+
+    // AMD. Register as an anonymous module.
+    define( [ "../widgets/datepicker" ], factory );
+  } else {
+
+    // Browser globals
+    factory( jQuery.datepicker );
+  }
+}( function( datepicker ) {
+
+  datepicker.regional.fr = {
+    closeText: "Fermer",
+    prevText: "Précédent",
+    nextText: "Suivant",
+    currentText: "Aujourd'hui",
+    monthNames: [ "janvier", "février", "mars", "avril", "mai", "juin",
+      "juillet", "août", "septembre", "octobre", "novembre", "décembre" ],
+    monthNamesShort: [ "janv.", "févr.", "mars", "avr.", "mai", "juin",
+      "juil.", "août", "sept.", "oct.", "nov.", "déc." ],
+    dayNames: [ "dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi" ],
+    dayNamesShort: [ "dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam." ],
+    dayNamesMin: [ "D","L","M","M","J","V","S" ],
+    weekHeader: "Sem.",
+    dateFormat: "dd/mm/yy",
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: "" };
+  datepicker.setDefaults( datepicker.regional.fr );
+
+  return datepicker.regional.fr;
+
+} ) );
