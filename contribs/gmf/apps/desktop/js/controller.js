3c7d5252df75f37b8011076f9937e3bc58daacee
Frederic Junod <frederic.junod@camptocamp.com>
2017-09-15 10:35:17 +0200

    Fix EPSG:2056 label

--- contribs/gmf/apps/desktop/js/controller.js
+++ contribs/gmf/apps/desktop/js/controller.js
@@ -78,7 +78,7 @@ app.DesktopController = function($scope, $injector) {
    */
   this.mousePositionProjections = [{
     code: 'EPSG:2056',
-    label: 'CH1903+ / LV03',
+    label: 'CH1903+ / LV95',
     filter: 'ngeoNumberCoordinates::{x}, {y} m'
   }, {
     code: 'EPSG:21781',
