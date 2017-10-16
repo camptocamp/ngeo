7a7c61b8c7cb63731c321b46ee80e99311f7d080
Frederic Junod <frederic.junod@camptocamp.com>
2017-10-16 11:17:52 +0200

    Change ngeo.Scalify filter to be stateful
    Port of #2987 for the 2.1 branch

--- src/services/filters.js
+++ src/services/filters.js
@@ -24,10 +24,12 @@ goog.require('ol.string');
  */
 ngeo.Scalify = function($filter) {
   var numberFilter = $filter('number');
-  return function(scale) {
+  var filterFn = function(scale) {
     var text = numberFilter(scale, 0);
     return text ? '1\u00a0:\u00a0' + text : '';
   };
+  filterFn['$stateful'] = true;
+  return filterFn;
 };
 
 ngeo.module.filter('ngeoScalify', ngeo.Scalify);
