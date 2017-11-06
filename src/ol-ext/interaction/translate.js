eff7e428b487deb5e9bf3f6bbff10ac3b295d1da
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2017-11-06 09:42:52 +0100

    Use pluggableMap in modifycircle after ol3 update

--- src/ol-ext/interaction/translate.js
+++ src/ol-ext/interaction/translate.js
@@ -114,7 +114,7 @@ ngeo.interaction.Translate.prototype.setActive = function(active) {
  * Remove the interaction from its current map and attach it to the new map.
  * Subclasses may set up event handlers to get notified about changes to
  * the map here.
- * @param {ol.Map} map Map.
+ * @param {ol.PluggableMap} map Map.
  * @override
  */
 ngeo.interaction.Translate.prototype.setMap = function(map) {
