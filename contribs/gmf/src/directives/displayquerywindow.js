5296f0f0ce6441003faa3c3d39fcd0dc315e2700
Frederic Junod <frederic.junod@camptocamp.com>
2017-11-13 10:36:58 +0100

    Clear query results on window/grid clear

--- contribs/gmf/src/directives/displayquerywindow.js
+++ contribs/gmf/src/directives/displayquerywindow.js
@@ -87,6 +87,7 @@ gmf.module.component('gmfDisplayquerywindow', gmf.displayquerywindowComponent);
 /**
  * @param {!angular.Scope} $scope Angular scope.
  * @param {!ngeox.QueryResult} ngeoQueryResult ngeo query result.
+ * @param {!ngeo.MapQuerent} ngeoMapQuerent ngeo map querent service.
  * @param {!ngeo.FeatureOverlayMgr} ngeoFeatureOverlayMgr The ngeo feature
  *     overlay manager service.
  * @constructor
@@ -95,7 +96,7 @@ gmf.module.component('gmfDisplayquerywindow', gmf.displayquerywindowComponent);
  * @ngdoc controller
  * @ngname GmfDisplayquerywindowController
  */
-gmf.DisplayquerywindowController = function($scope, ngeoQueryResult,
+gmf.DisplayquerywindowController = function($scope, ngeoQueryResult, ngeoMapQuerent,
   ngeoFeatureOverlayMgr) {
 
   /**
@@ -134,6 +135,12 @@ gmf.DisplayquerywindowController = function($scope, ngeoQueryResult,
     total: 0,
     pending: false
   };
+
+  /**
+   * @type {ngeo.MapQuerent}
+   * @private
+   */
+  this.ngeoMapQuerent_ = ngeoMapQuerent;
 
   /**
    * @type {?ngeox.QueryResultSource}
@@ -477,6 +484,7 @@ function(opt_lastFeature) {
 gmf.DisplayquerywindowController.prototype.close = function() {
   this.open = false;
   this.clear();
+  this.ngeoMapQuerent_.clear();
 };
 
 
