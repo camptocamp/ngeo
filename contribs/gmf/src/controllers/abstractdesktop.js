6eb6b74b76509e0e9d56a31dc935ea278c0e6210
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2017-11-23 09:51:58 +0100

    GMF Print: Move from directive to component

--- contribs/gmf/src/controllers/abstractdesktop.js
+++ contribs/gmf/src/controllers/abstractdesktop.js
@@ -19,7 +19,7 @@ goog.require('gmf.filterselectorComponent');
 /** @suppress {extraRequire} */
 goog.require('gmf.mousepositionComponent');
 /** @suppress {extraRequire} */
-goog.require('gmf.printDirective');
+goog.require('gmf.printComponent');
 /** @suppress {extraRequire} */
 goog.require('gmf.profileComponent');
 /** @suppress {extraRequire} */
