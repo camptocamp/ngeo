0c71a2be6d2a82204bdf2a68fc72afd9a692c112
Stéphane Brunner <stephane.brunner@camptocamp.com>
2017-11-23 08:26:14 +0100

    Fix tests

--- src/modules/import/filemodule.js
+++ src/modules/import/filemodule.js
@@ -2,6 +2,7 @@ goog.module('ngeo.fileService');
 goog.module.declareLegacyNamespace();
 
 goog.require('ngeo.File');
+goog.require('ngeo');
 
 exports.module = angular.module('ngeo.fileService', []);
 
@@ -9,3 +10,4 @@ exports.module = angular.module('ngeo.fileService', []);
  * Read/load a file then returns the content.
  */
 exports.module.service('ngeoFile', ngeo.File);
+ngeo.module.requires.push(exports.module.name);
