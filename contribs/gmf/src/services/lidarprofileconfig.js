6cd344d13bfc91c4265c0a060d853e8bba2cb2d0
monodo <olivier.monod@ne.ch>
2017-11-29 10:59:30 +0100

    get the profile config from pytree as one single json

--- contribs/gmf/src/services/lidarprofileconfig.js
+++ contribs/gmf/src/services/lidarprofileconfig.js
@@ -24,109 +24,39 @@ gmf.LidarProfileConfig = function($http, pytreeLidarProfileJsonUrl) {
   this.profileConfig.currentScaleY = {};
   this.profileConfig.previousDomain = [];
   this.profileConfig.pointAttributes = {};
-  
-  // TODO get this params from pytree server
   this.profileConfig.currentZoom = 1;
   this.profileConfig.distanceOffset = 0;
-  this.profileConfig.initialLOD = 5;
-  this.profileConfig.pointSize = 1;
   this.profileConfig.margin = {
     'left': 40,
     'top': 10,
     'right': 200,
     'bottom': 40
   };
-  this.profileConfig.defaultColor = 'RGB(250,150,150)';
+  // snap tolerance for d3 profile highlight
   this.profileConfig.tolerance= 5;
 
 };
 
 gmf.LidarProfileConfig.prototype.initProfileConfig = function() {
-  this.getClassificationColors();
-  this.getSelectedMaterial();
-  this.getMaterials();
-  this.getWidth();
-  this.getPointAttributes();
-  this.getMinLOD();
-  this.getMaxLOD();
+  this.getConfig();
 }
 
-gmf.LidarProfileConfig.prototype.getClassificationColors = function() {
-  this.$http_.get(this.pytreeLidarProfileJsonUrl_ + '/get_classification_colors').then((resp) => {
-    this.profileConfig.classification = resp.data;
+gmf.LidarProfileConfig.prototype.getConfig = function() {
+  this.$http_.get(this.pytreeLidarProfileJsonUrl_ + '/profile_config_gmf2').then((resp) => {
+
+    this.profileConfig.classification = resp.data['classification_colors'];
+    this.profileConfig.profilWidth = resp.data['width'];
+    this.profileConfig.minLOD = resp.data['minLOD'];
+    this.profileConfig.maxLOD = resp.data['maxLOD'];
+    this.profileConfig.initialLOD = resp.data['initialLOD'];
+    this.profileConfig.pointSize = resp.data['point_size'];
+    this.profileConfig.pointAttributes = resp.data['point_attributes'];
+    this.profileConfig.defaultAttribute = resp.data['default_attribute'];
+    this.profileConfig.defaultPointCloud = resp.data['default_point_cloud'];
+    this.profileConfig.defaultColor = resp.data['default_color'];
+    this.profileConfig.pointClouds = resp.data['pointclouds'];
+    console.log(this.profileConfig);
   });
-};
-
-gmf.LidarProfileConfig.prototype.getMaterials = function() {
-
-  this.profileConfig.materials = [
-    {'name': 'COLOR_PACKED', 'value': 'COLOR_PACKED', 'selected': ''},
-    {'name': 'INTENSITY', 'value': 'INTENSITY', 'selected': ''},
-    {'name': 'CLASSIFICATION',  'value': 'CLASSIFICATION','selected': ''}
-  ]
-};
-
-gmf.LidarProfileConfig.prototype.getSelectedMaterial = function() {
-  this.$http_.get(this.pytreeLidarProfileJsonUrl_ + '/get_default_material').then((resp) => {
-    this.profileConfig.selectedMaterial = resp.data;
-  });
-};
-
-gmf.LidarProfileConfig.prototype.getWidth = function() {
-  this.profileConfig.profilWidth = 20;
-};
-
-gmf.LidarProfileConfig.prototype.getMinLOD = function() {
-  this.profileConfig.minLOD = 0;
-};
-
-gmf.LidarProfileConfig.prototype.getMaxLOD = function() {
-  this.profileConfig.maxLOD = 5;
-};
-
-gmf.LidarProfileConfig.prototype.getPointAttributes = function() {
-
-  this.profileConfig.pointAttributes.POSITION_CARTESIAN = {
-    name: 'POSITION_CARTESIAN',
-    elements: 3,
-    bytes: 12
-  }
-
-  this.profileConfig.pointAttributes.POSITION_PROJECTED_PROFILE = {
-    name: 'POSITION_PROJECTED_PROFILE',
-    elements: 2,
-    bytes: 8
-  }
-
-  this.profileConfig.pointAttributes.COLOR_PACKED = {
-    name: 'COLOR_PACKED',
-    elements: 4,
-    bytes: 4
-  }
-
-  this.profileConfig.pointAttributes.RGB = {
-    name: 'RGB',
-    elements: 3,
-    bytes: 3
-  }
-
-  this.profileConfig.pointAttributes.RGBA = {
-    name: 'RGBA',
-    elements: 4,
-    bytes: 4
-  }
-
-  this.profileConfig.pointAttributes.INTENSITY = {
-    name: 'INTENSITY',
-    elements: 1,
-    bytes: 2
-  }
-
-  this.profileConfig.pointAttributes.CLASSIFICATION = {
-    name: 'CLASSIFICATION',
-    elements: 1,
-    bytes: 1
-  }
 };
 
 gmf.module.service('gmfLidarProfileConfig', gmf.LidarProfileConfig);
