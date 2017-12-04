158a5518520023ea4eca85e5bc4855121cc59d73
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2017-12-04 16:23:37 +0100

    Move ngeo recenter to the map module

--- src/map/module.js
+++ src/map/module.js
@@ -5,11 +5,13 @@ goog.provide('ngeo.map.module');
 
 goog.require('ngeo');
 goog.require('ngeo.map.directive');
+goog.require('ngeo.map.recenter');
 
 /**
  * @type {!angular.Module}
  */
 ngeo.map.module = angular.module('ngeoMapModule', [
   ngeo.module.name, // Change me when all dependencies are in a module.
-  ngeo.map.directive.name
+  ngeo.map.directive.name,
+  ngeo.map.recenter.name
 ]);
