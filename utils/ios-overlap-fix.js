ba41e453158e5b4af6c2fe186d5295a30c6cb4d0
Laurent Lienher <laurent.lienher@camptocamp.com>
2017-12-15 13:49:37 +0100

    Fix IE

--- utils/ios-overlap-fix.js
+++ utils/ios-overlap-fix.js
@@ -1,30 +1,32 @@
-// Hack for ios Safari browser (UI overlapping on iOS 10)
-const userAgent = window.navigator.userAgent;
-const regEx = /iP(hone|od|ad)/;
-const iOS = regEx.test(userAgent);
-const webkit = /WebKit/i.test(userAgent);
-const chromeiOS = /(Chrome|CriOS|OPiOS)/i.test(userAgent);
-const platform = regEx.test(window.navigator.platform);
+(function() {
+  // Hack for ios Safari browser (UI overlapping on iOS 10)
+  var userAgent = window.navigator.userAgent;
+  var regEx = /iP(hone|od|ad)/;
+  var iOS = regEx.test(userAgent);
+  var webkit = /WebKit/i.test(userAgent);
+  var chromeiOS = /(Chrome|CriOS|OPiOS)/i.test(userAgent);
+  var platform = regEx.test(window.navigator.platform);
 
-const iosChecker = function() {
-  const v = (window.navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
-  return [parseInt(v[1], 10), parseInt(v  [2], 10), parseInt(v[3] || 0, 10)];
-};
+  var iosChecker = function() {
+    var v = (window.navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+    return [parseInt(v[1], 10), parseInt(v  [2], 10), parseInt(v[3] || 0, 10)];
+  };
 
-/**
-* iOS is true on iOS or emulators (user-agent)
-* webkit is true on Safari/Chrome, etc
-* platform is true on iOS, not emulator (platform)
-* chromeiOS is true on other browsers (chrome/opera)
-*/
-if (iOS && webkit && platform && !chromeiOS) {
-  const iosVersion = iosChecker()[0];
-  if (iosVersion >= 10) {
-    const interval = setInterval(() => {
-      if ($('div.ol-zoom').get(0)) {
-        clearInterval(interval);
-        $('body').addClass('ios-margin');
-      }
-    }, 100);
+  /**
+  * iOS is true on iOS or emulators (user-agent)
+  * webkit is true on Safari/Chrome, etc
+  * platform is true on iOS, not emulator (platform)
+  * chromeiOS is true on other browsers (chrome/opera)
+  */
+  if (iOS && webkit && platform && !chromeiOS) {
+    var iosVersion = iosChecker()[0];
+    if (iosVersion >= 10) {
+      var interval = setInterval(function() {
+        if ($('div.ol-zoom').get(0)) {
+          clearInterval(interval);
+          $('body').addClass('ios-margin');
+        }
+      }, 100);
+    }
   }
-}
+})();
