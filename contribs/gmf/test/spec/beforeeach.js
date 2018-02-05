9aeb17d35b77870a639bc3fcb8dd0e56ad782c10
Stéphane Brunner <stephane.brunner@camptocamp.com>
2018-02-05 12:25:57 +0100

    Refactor beforeeach

--- contribs/gmf/test/spec/beforeeach.js
+++ contribs/gmf/test/spec/beforeeach.js
@@ -2,26 +2,32 @@ goog.require('gmf.controllers.defaultConfig');
 
 goog.require('gmf.contextualdata.component');
 goog.require('gmf.layertree.module');
+goog.require('gmf.map.component');
 goog.require('gmf.permalink.module');
 goog.require('gmf.print.component');
 goog.require('gmf.profile.module');
 goog.require('gmf.query.extraModule');
 
+const appModule = angular.module('gmfapp', []);
+
+beforeEach(() =>  {
+  appModule.requires.push(gmf.controllers.defaultConfig.name);
+  appModule.requires.push(gmf.contextualdata.component.name);
+  appModule.requires.push(gmf.map.component.name);
+  appModule.requires.push(gmf.layertree.module.name);
+  appModule.requires.push(gmf.permalink.module.name);
+  appModule.requires.push(gmf.print.component.name);
+  appModule.requires.push(gmf.profile.module.name);
+  appModule.requires.push(gmf.query.extraModule.name);
+});
+
+beforeEach(angular.mock.module('gmfapp'));
+
 beforeEach(() => {
-  const gmfModule = angular.module('gmfDefaultConfig');
-  gmfModule.constant('angularLocaleScript', 'http://fake');
-  gmfModule.constant('gmfLayersUrl', 'https://fake');
+  gmf.controllers.defaultConfig.constant('angularLocaleScript', 'http://fake');
+  gmf.controllers.defaultConfig.constant('gmfLayersUrl', 'https://fake');
 
-  Array.prototype.push.apply(gmfModule.requires, [
-    gmf.contextualdata.component.name,
-    gmf.layertree.module.name,
-    gmf.permalink.module.name,
-    gmf.print.component.name,
-    gmf.profile.module.name,
-    gmf.query.extraModule.name,
-  ]);
-
-  angular.mock.module('gmfDefaultConfig', ($provide) => {
+  angular.mock.module('gmfapp', ($provide) => {
     $provide.value('gmfTreeUrl', 'http://fake/gmf/themes');
     $provide.value('gmfShortenerCreateUrl', 'http://fake/gmf/short/create');
     $provide.value('authenticationBaseUrl', 'https://fake/gmf/authentication');
