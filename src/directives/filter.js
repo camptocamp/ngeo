3af8ebe87d22ef618e378c8d906b521c95bf7966
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2018-03-14 10:02:08 +0100

    Improve filter buttons activation
    For GEO-1053

--- src/directives/filter.js
+++ src/directives/filter.js
@@ -198,6 +198,15 @@ ngeo.FilterController = class {
 
 
   /**
+   * @return {boolean} True if at least one rule is currently defined.
+   * @export
+   */
+  hasARule() {
+    return [].concat(this.customRules, this.directedRules).length > 0;
+  }
+
+
+  /**
    * Loop in all directed and custom rules. Apply the rules that have a proper
    * value inside the data source, in the `filterRules` property.
    * @export
