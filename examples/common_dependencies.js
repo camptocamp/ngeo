e0ca83be82873ad4d32f27d92b65e9ed4fe1829d
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2018-04-27 11:32:59 +0200

    Handle https redirect in common dependency files

--- examples/common_dependencies.js
+++ examples/common_dependencies.js
@@ -5,3 +5,12 @@ import '../utils/watchwatchers.js';
 
 import 'openlayers/css/ol.css';
 import 'bootstrap/dist/css/bootstrap.css';
+
+/*
+ * Auto redirect to https to prevent CORS exceptions
+ */
+if (window.location.protocol === 'http:' && window.location.host !== 'localhost') {
+  const restOfUrl = window.location.href.substr(5);
+  /** @type {Location} */
+  window.location = `https:${restOfUrl}`;
+}
