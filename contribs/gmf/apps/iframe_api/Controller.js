1a01c3ff8029f36f439c12335e4fce93d94aeed8
Alexandre Dubé <adube@mapgears.com>
2018-10-31 08:38:13 -0400

    Fix 617 - Iframe API new app (#4339)
    * Fix 617 - Iframe API new app

---
+++ contribs/gmf/apps/iframe_api/Controller.js
@@ -0,0 +1,66 @@
+/**
+ * @module app.iframe_api.Controller
+ */
+/**
+ * Application entry point.
+ *
+ * This file includes `goog.require`'s for all the components/directives used
+ * by the HTML page and the controller to provide the configuration.
+ */
+
+import './sass/iframe_api.scss';
+import gmfControllersAbstractAPIController from 'gmf/controllers/AbstractAPIController.js';
+import appBase from '../appmodule.js';
+import ngeoProjEPSG2056 from 'ngeo/proj/EPSG2056.js';
+import ngeoProjEPSG21781 from 'ngeo/proj/EPSG21781.js';
+import * as olBase from 'ol/index.js';
+import Raven from 'raven-js/src/raven.js';
+import RavenPluginsAngular from 'raven-js/plugins/angular.js';
+
+if (!window.requestAnimationFrame) {
+  alert('Your browser is not supported, please update it or use another one. You will be redirected.\n\n'
+    + 'Votre navigateur n\'est pas supporté, veuillez le mettre à jour ou en utiliser un autre. Vous allez être redirigé.\n\n'
+    + 'Ihr Browser wird nicht unterstützt, bitte aktualisieren Sie ihn oder verwenden Sie einen anderen. Sie werden weitergeleitet.');
+  window.location = 'http://geomapfish.org/';
+}
+
+/**
+ * @param {angular.Scope} $scope Scope.
+ * @param {angular.$injector} $injector Main injector.
+ * @constructor
+ * @extends {gmf.controllers.AbstractAPIController}
+ * @ngInject
+ * @export
+ */
+const exports = function($scope, $injector) {
+  gmfControllersAbstractAPIController.call(this, {
+    srid: 21781,
+    mapViewConfig: {
+      center: [632464, 185457],
+      zoom: 3,
+      resolutions: [250, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05]
+    }
+  }, $scope, $injector);
+
+  this.ngeoProjEPSG2056 = ngeoProjEPSG2056;
+  this.ngeoProjEPSG21781 = ngeoProjEPSG21781;
+
+  if ($injector.has('sentryUrl')) {
+    const options = $injector.has('sentryOptions') ? $injector.get('sentryOptions') : undefined;
+    const raven = new Raven();
+    raven.config($injector.get('sentryUrl'), options)
+      .addPlugin(RavenPluginsAngular)
+      .install();
+  }
+};
+
+olBase.inherits(exports, gmfControllersAbstractAPIController);
+
+exports.module = angular.module('Appiframe_api', [
+  appBase.module.name,
+  gmfControllersAbstractAPIController.module.name,
+]);
+
+exports.module.controller('IframeAPIController', exports);
+
+export default exports;
