cdb85bebd76b98ea2b33ad895b37595297a62887
Frederic Junod <frederic.junod@camptocamp.com>
2019-03-13 12:48:43 +0100

    Remove unneeded supportedProjections config property

--- api/src/constants.js
+++ api/src/constants.js
@@ -1,11 +1,9 @@
 import EPSG21781 from '@geoblocks/proj/src/EPSG_21781.js';
-import EPSG2056 from '@geoblocks/proj/src/EPSG_2056.js';
 
 
 export default {
   themesUrl: undefined,
   projection: EPSG21781,
-  supportedProjections: [EPSG21781, EPSG2056],
   resolutions: [250, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05],
   extent: [420000, 30000, 660000, 350000],
   /**
