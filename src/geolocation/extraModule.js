8886b2e7073fd755ee422aa9b02c4e02beebb18d
Frederic Junod <frederic.junod@camptocamp.com>
2019-07-15 16:40:24 +0200

    Remove desktop geolocation

--- src/geolocation/extraModule.js
+++ src/geolocation/extraModule.js
@@ -1,13 +1,11 @@
 /**
  */
 import angular from 'angular';
-import ngeoGeolocationDesktop from 'ngeo/geolocation/desktop.js';
 import ngeoGeolocationMobile from 'ngeo/geolocation/mobile.js';
 
 /**
  * @type {angular.IModule}
  */
 export default angular.module('ngeoGeolocationExtraModule', [
-  ngeoGeolocationDesktop.name,
   ngeoGeolocationMobile.name
 ]);
