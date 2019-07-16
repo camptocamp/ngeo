7ed0923216f86cc304a9193d4bcde2725ec8a536
Stéphane Brunner <stephane.brunner@camptocamp.com>
2019-07-16 14:40:30 +0200

    Add --no-sandbox argument
    To be able to run it as root

--- buildtools/check-example.js
+++ buildtools/check-example.js
@@ -65,7 +65,7 @@ function loaded(page, browser) {
   }, 500);
 }
 (async () => {
-  const browser = await puppeteer.launch({args: ['--disable-web-security']});
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-web-security']});
   const page = await browser.newPage();
   await page.setViewport({width: 1920, height: 1080});
   await page.setRequestInterception(true);
