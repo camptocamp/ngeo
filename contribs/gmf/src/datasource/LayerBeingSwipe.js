2f3a18d60d187fa81c4e9792cdb5134ff38b2730
Rosalie <rcote@mapgears.com>
2019-08-26 11:31:55 -0400

    Introduces swipe element on desktop alt

---
+++ contribs/gmf/src/datasource/LayerBeingSwipe.js
@@ -0,0 +1,19 @@
+import angular from 'angular';
+
+
+/**
+ * @typedef {Object} LayerBeingSwipe
+ * @property {import('ol/Map.js').default} map
+ * @property {import('ol/layer/Tile.js').default} layer
+ */
+
+/**
+ * @type {angular.IModule}
+ * @hidden
+ */
+const module = angular.module('gmfLayerBeingSwipe', []);
+module.value('gmfLayerBeingSwipe', {
+  layer: null
+});
+
+export default module;
