4ef88e82605d457e9536a0cd1f0348d19e62afa6
Stéphane Brunner <stephane.brunner@camptocamp.com>
2019-10-02 13:59:57 +0200

    log

--- .github/pr-message.js
+++ .github/pr-message.js
@@ -5,7 +5,11 @@ async function run() {
   // Get client and context
   const client = new github.GitHub(process.env.GITHUB_TOKEN);
   const context = github.context;
-  for (const commit in client.pulls.listCommits()) {
+  for (const commit in await client.pulls.listCommits({
+    owner: context.issue.owner,
+    repo: context.issue.repo,
+    pull_number: context.issue.number,
+  })) {
     console.log(Object.keys(commit));
     console.log(commit);
   }
