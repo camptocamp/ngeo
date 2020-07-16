c7a362055c3b5948557ed8641dc95e85818d08e4
Stéphane Brunner <stephane.brunner@camptocamp.com>
2020-07-16 10:41:07 +0200

    Update the examples

--- contribs/gmf/apps/desktop/Controller.js
+++ contribs/gmf/apps/desktop/Controller.js
@@ -81,7 +81,20 @@ class Controller extends AbstractDesktopController {
     /**
      * @type {number[]}
      */
-    this.scaleSelectorValues = [250000, 100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 250, 100, 50];
+    this.scaleSelectorValues = [
+      1000000,
+      500000,
+      200000,
+      100000,
+      50000,
+      20000,
+      10000,
+      5000,
+      2000,
+      1000,
+      500,
+      200,
+    ];
 
     /**
      * @type {string[]}
