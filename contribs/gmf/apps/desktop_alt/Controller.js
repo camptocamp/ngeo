2bded4b763d71aac284ce0ebb96d26ea061f5f3a
Stéphane Brunner <stephane.brunner@camptocamp.com>
2020-07-16 10:41:07 +0200

    Update the examples

--- contribs/gmf/apps/desktop_alt/Controller.js
+++ contribs/gmf/apps/desktop_alt/Controller.js
@@ -96,11 +96,6 @@ class Controller extends AbstractDesktopController {
     this.showInfobar = true;
 
     /**
-     * @type {number[]}
-     */
-    this.scaleSelectorValues = [250000, 100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 250, 100, 50];
-
-    /**
      * @type {string[]}
      */
     this.elevationLayers = ['srtm-partial'];
