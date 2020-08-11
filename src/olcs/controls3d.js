2c5ed5a33df401efca7dda1f2cc39a2697b9005c
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2020-08-11 17:20:17 +0200

    Workaround OL constraint bug

--- src/olcs/controls3d.js
+++ src/olcs/controls3d.js
@@ -254,7 +254,9 @@ const Controller = class {
     }
     const view = this.ol3dm.getOlView();
     const cur = view.getResolution();
-    const newResolution = view.getConstrainedResolution(cur, delta);
+    // resolution constraint looks broken in OL
+    // Using a slightly larger delta than 1 workaround the issue.
+    const newResolution = view.getConstrainedResolution(cur, -delta * 1.01);
     if (view.getAnimating()) {
       view.cancelAnimations();
     }
