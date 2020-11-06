b38bfd30bff8a62390961a95b946b66a19bc5c8d
Rémi Desgrange <remi.desgrange@camptocamp.com>
2020-11-06 16:36:17 +0100

    Adding ability to specify a layer in measure.
    This is work related to luxembourg geoportal

--- src/interaction/Measure.js
+++ src/interaction/Measure.js
@@ -202,10 +202,13 @@ class Measure extends olInteractionInteraction {
      * @type {import("ol/layer/Vector.js").default}
      * @private
      */
-    this.vectorLayer_ = new olLayerVector({
-      source: new olSourceVector(),
-      style: style,
-    });
+    this.vectorLayer_ =
+      options.layer !== undefined
+        ? options.layer
+        : new olLayerVector({
+            source: new olSourceVector(),
+            style: style,
+          });
 
     const source = this.vectorLayer_.getSource();
     if (!(source instanceof VectorSource)) {
