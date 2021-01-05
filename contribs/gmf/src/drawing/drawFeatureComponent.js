b39047f0fed2ff585e39cce994c8a00c4321b3cd
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2021-01-05 11:30:04 +0100

    Update Copyright to 2021

--- contribs/gmf/src/drawing/drawFeatureComponent.js
+++ contribs/gmf/src/drawing/drawFeatureComponent.js
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2016-2020 Camptocamp SA
+// Copyright (c) 2016-2021 Camptocamp SA
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
@@ -125,7 +125,14 @@ module.directive('gmfDrawfeature', drawinfDrawFeatureComponent);
  * @ngdoc controller
  * @ngname GmfDrawfeatureController
  */
-export function Controller($scope, $timeout, gettextCatalog, ngeoFeatureHelper, ngeoFeatures, ngeoToolActivateMgr) {
+export function Controller(
+  $scope,
+  $timeout,
+  gettextCatalog,
+  ngeoFeatureHelper,
+  ngeoFeatures,
+  ngeoToolActivateMgr
+) {
   /**
    * @type {?import("ol/Map.js").default}
    */
