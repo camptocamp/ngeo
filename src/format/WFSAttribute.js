cde985f0080b040268a92f5afe99de9b8117f6bb
RenataMuellerC2C <62992955+RenataMuellerC2C@users.noreply.github.com>
2021-01-29 09:42:50 +0100

    Change attribute types to ngeoFormatAttributeTypes (#6746)

--- src/format/WFSAttribute.js
+++ src/format/WFSAttribute.js
@@ -42,6 +42,30 @@ export default class {
   }
 
   /**
+   * @param {import('ngeo/format/Attribute.js').Attribute} attribute
+   * @param {string} type the type
+   */
+  setAttributeType(attribute, type) {
+    if (type === 'gml:TimeInstantType' || type === 'dateTime') {
+      attribute.type = ngeoFormatAttributeType.DATETIME;
+    } else if (type === 'date') {
+      attribute.type = ngeoFormatAttributeType.DATE;
+    } else if (type === 'time') {
+      attribute.type = ngeoFormatAttributeType.TIME;
+    } else if (type === 'decimal' || type === 'double') {
+      attribute.type = ngeoFormatAttributeType.NUMBER;
+      attribute.numType = FormatNumberType.FLOAT;
+    } else if (type === 'integer' || type === 'long') {
+      attribute.type = ngeoFormatAttributeType.NUMBER;
+      attribute.numType = FormatNumberType.INTEGER;
+    } else if (type === 'boolean') {
+      attribute.type = ngeoFormatAttributeType.BOOLEAN;
+    } else {
+      attribute.type = ngeoFormatAttributeType.TEXT;
+    }
+  }
+
+  /**
    * @param {Object} object Complex type element
    * @return {import('ngeo/format/Attribute.js').Attribute} Attribute
    * @private
@@ -61,23 +85,7 @@ export default class {
     const type = object.type;
 
     if (!setGeometryType(attribute, type)) {
-      if (type === 'gml:TimeInstantType' || type === 'dateTime') {
-        attribute.type = ngeoFormatAttributeType.DATETIME;
-      } else if (type === 'date') {
-        attribute.type = ngeoFormatAttributeType.DATE;
-      } else if (type === 'time') {
-        attribute.type = ngeoFormatAttributeType.TIME;
-      } else if (type === 'decimal' || type === 'double') {
-        attribute.type = ngeoFormatAttributeType.NUMBER;
-        attribute.numType = FormatNumberType.FLOAT;
-      } else if (type === 'integer' || type === 'long') {
-        attribute.type = ngeoFormatAttributeType.NUMBER;
-        attribute.numType = FormatNumberType.INTEGER;
-      } else if (type === 'boolean') {
-        attribute.type = ngeoFormatAttributeType.BOOLEAN;
-      } else {
-        attribute.type = ngeoFormatAttributeType.TEXT;
-      }
+      this.setAttributeType(attribute, type);
     }
 
     return attribute;
