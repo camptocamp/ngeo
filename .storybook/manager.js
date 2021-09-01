2b036444f932b9e4c07b99dc702d7f267e1302f4
Stéphane Brunner <stephane.brunner@camptocamp.com>
2021-09-01 14:35:18 +0200

    Fix first load

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,6 @@
 // Storybook layout configuration.
 
-import { addons } from '@storybook/addons';
+import {addons} from '@storybook/addons';
 
 // Default layout config.
 // Set panelPosition to 'right' to avoid layout issues on opening the browser dev tools.
@@ -19,18 +19,19 @@ addons.setConfig({
     collapsedRoots: ['other'],
   },
   toolbar: {
-    title: { hidden: false, },
-    zoom: { hidden: false, },
-    eject: { hidden: false, },
-    copy: { hidden: false, },
-    fullscreen: { hidden: false, },
+    title: {hidden: false},
+    zoom: {hidden: false},
+    eject: {hidden: false},
+    copy: {hidden: false},
+    fullscreen: {hidden: false},
   },
 });
 
 // Set default width of the panel to have more space.
 // See https://github.com/storybookjs/storybook/issues/9682
-const storybookLayout = JSON.parse(localStorage["storybook-layout"]);
-if (storybookLayout) {
-  const newLayout = { resizerPanel: { x: window.innerWidth - 400, y: 0 }};
-  localStorage["storybook-layout"] = JSON.stringify({...storybookLayout, ...newLayout});
+if (!localStorage['storybook-layout']) {
+  localStorage['storybook-layout'] = '{}';
 }
+const storybookLayout = JSON.parse(localStorage['storybook-layout']);
+const newLayout = {resizerPanel: {x: window.innerWidth - 400, y: 0}};
+localStorage['storybook-layout'] = JSON.stringify({...storybookLayout, ...newLayout});
