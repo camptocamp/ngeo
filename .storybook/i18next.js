934b81329514fa52a904c0f2cb3a203aa27afafd
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2021-09-14 11:06:12 +0200

    Add i18n to Storybook

---
+++ .storybook/i18next.js
@@ -0,0 +1,31 @@
+import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+const ns = ['app'];
+const supportedLngs = ['en', 'fr'];
+
+i18next.use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        //debug: true,
+        lng: 'en',
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false,
+        },
+        defaultNS: 'app',
+        ns,
+        supportedLngs,
+    });
+
+supportedLngs.forEach((lang) => {
+    ns.forEach((n) => {
+        i18next.addResources(
+            lang,
+            n,
+            require(`../locales/${lang}/${n}.json`)
+        );
+    });
+});
+
+export {i18next};
