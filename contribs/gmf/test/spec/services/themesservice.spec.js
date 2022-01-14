6e2df473cb7955754165c25651e232aaef935901
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2022-01-14 15:39:01 +0100

    Update themes used in tests

--- contribs/gmf/test/spec/services/themesservice.spec.js
+++ contribs/gmf/test/spec/services/themesservice.spec.js
@@ -55,7 +55,7 @@ describe('gmf.theme.Themes', () => {
     $httpBackend.expectGET(treeUrl);
     gmfTestDataThemes.background_layers.forEach((bgLayer) => {
       const response =
-        bgLayer.name == 'OSM' ? gmfTestDataThemescapabilities.map : gmfTestDataThemescapabilities.asitvd;
+        bgLayer.name.includes('asitvd') ? gmfTestDataThemescapabilities.asitvd : gmfTestDataThemescapabilities.map;
       $httpBackend.when('GET', bgLayer.url).respond(response);
       if (!urls.includes(bgLayer.url)) {
         urls.push(bgLayer.url);
