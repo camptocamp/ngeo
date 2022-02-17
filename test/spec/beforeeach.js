dbd622e78f62d6b455461b68c06ee919b4317b0d
Stéphane Brunner <stephane.brunner@camptocamp.com>
2022-02-17 11:58:18 +0100

    Add i18next in tests

--- test/spec/beforeeach.js
+++ test/spec/beforeeach.js
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2014-2021 Camptocamp SA
+// Copyright (c) 2014-2022 Camptocamp SA
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
@@ -23,6 +23,7 @@
 /* eslint valid-jsdoc: 0 */
 import angular from 'angular';
 import ngeoMainmodule from 'ngeo/mainmodule';
+import i18next from 'i18next';
 
 const myModule = angular.module('app', []);
 
@@ -89,4 +90,10 @@ beforeEach(() => {
   });
 });
 
+beforeEach(() => {
+  i18next.init({
+    debug: true,
+    lng: 'en',
+  });
+});
 export default myModule;
