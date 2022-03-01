85e5f53eeac761d61035978bf64183abfedce93b
Laurent Lienher <laurent.lienher@camptocamp.com>
2022-03-01 14:30:12 +0100

    Set the print panel closed when we reduce it

--- contribs/gmf/src/controllers/AbstractDesktopController.js
+++ contribs/gmf/src/controllers/AbstractDesktopController.js
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2016-2021 Camptocamp SA
+// Copyright (c) 2016-2022 Camptocamp SA
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
@@ -453,6 +453,7 @@ export class AbstractDesktopController extends AbstractAPIController {
       this.toolsActive = false;
       this.loginActive = false;
       this.printActive = false;
+      this.printPanelActive = false;
       this.drawFeatureActive = false;
       this.filterSelectorActive = false;
       this.editFeatureActive = false;
