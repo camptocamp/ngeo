acab3874443bb284e02cc7c39961de14893067c3
Laurent Lienher <laurent.lienher@camptocamp.com>
2022-05-05 13:30:04 +0200

    Fix CSV export with undefined packed_color attribute error

--- contribs/gmf/src/lidarprofile/Manager.js
+++ contribs/gmf/src/lidarprofile/Manager.js
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2018-2021 Camptocamp SA
+// Copyright (c) 2018-2022 Camptocamp SA
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
@@ -471,7 +471,7 @@ export class LidarprofileManager {
           const intensity = view.getUint8(aoffset);
           points.intensity.push(intensity);
           this.profilePoints.intensity.push(intensity);
-        } else if (attribute.value == 'COLOR_PACKED') {
+        } else if (attribute.value == 'RGBA') {
           const r = view.getUint8(aoffset);
           const g = view.getUint8(aoffset + 1);
           const b = view.getUint8(aoffset + 2);
