4328de02973c8ecd34fc6742fd7f5361e6f06eb9
Stéphane Brunner <stephane.brunner@camptocamp.com>
2022-05-18 18:00:00 +0200

    Generic LIDAR panel title translation key

--- src/lidar/PanelElement.ts
+++ src/lidar/PanelElement.ts
@@ -143,7 +143,7 @@ export default class GmfLidarPanel extends ToolPanelElement {
       <style>
         ${unsafeCSS(this.customCSS_)}
       </style>
-      ${this.getTitle(i18next.t('LIDAR profile (only on the canton of Neuchâtel)'))}
+      ${this.getTitle(i18next.t('LIDAR profile'))}
       <div class="lidar-panel" gmf-drawprofileline>
         <p>
           <button class="btn btn-default ${this.drawlineClass}" @click=${() => this.toggleDrawLine()}>
