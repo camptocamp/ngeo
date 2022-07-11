a5e6a70d10f732d928017c6c848bd0352a4e56db
Stéphane Brunner <stephane.brunner@camptocamp.com>
2022-07-11 10:06:59 +0200

    Remove no more working Angular Integration

--- buildtools/webpack.config.dll.js
+++ buildtools/webpack.config.dll.js
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2019-2021 Camptocamp SA
+// Copyright (c) 2019-2022 Camptocamp SA
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
@@ -79,7 +79,6 @@ module.exports = {
   resolve: {
     alias: {
       'jquery-ui/datepicker': 'jquery-ui/ui/widgets/datepicker', // For angular-ui-date
-      '@sentry/integrations': '@sentry/integrations/dist/angular.js',
     },
   },
   optimization: {
