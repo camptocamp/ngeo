6b20d144d2aa6901229d1baa95ba0957f83ea63e
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2023-01-24 15:10:27 +0100

    Add username to print spec (#8987)

--- src/auth/service.ts
+++ src/auth/service.ts
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2016-2022 Camptocamp SA
+// Copyright (c) 2016-2023 Camptocamp SA
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
@@ -293,6 +293,13 @@ export class AuthenticationService {
     return fetch(url, options).then((resp) =>
       resp.json().then((data: AuthenticationDefaultResponse) => data)
     );
+  }
+
+  /**
+   * @returns Username
+   */
+  getUsername(): string | null {
+    return this.user_.username || null;
   }
 
   /**
