cc38ac970012bc370b5dfc4d4435957b0d0821b8
Stéphane Brunner <stephane.brunner@camptocamp.com>
2025-01-31 13:48:43 +0100

    Fix OIDC login on mobile
    When the openIdConnectBaseUrl is modified, the openIdConnectUrl should
    also be updated.

--- src/auth/FormElement.ts
+++ src/auth/FormElement.ts
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2021-2024 Camptocamp SA
+// Copyright (c) 2021-2025 Camptocamp SA
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
@@ -126,6 +126,7 @@ export default class GmfAuthForm extends GmfBaseElement {
     if (configuration.gmfCustomCSS && configuration.gmfCustomCSS.authentication !== undefined) {
       this.customCSS_ = configuration.gmfCustomCSS.authentication;
     }
+    this._updateOpenIdConnectUrl();
   }
 
   static styles: CSSResult[] = [
