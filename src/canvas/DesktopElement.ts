1f4b56186a3037383b294e47e8c73555cd443fcd
Renata Müller <renata.mueller@camptocamp.com>
2026-03-26 16:58:56 +0100

    Keep tool panel resizable when data panel closed

--- src/canvas/DesktopElement.ts
+++ src/canvas/DesktopElement.ts
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2022-2025 Camptocamp SA
+// Copyright (c) 2022-2026 Camptocamp SA
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
@@ -561,7 +561,7 @@ export default class GmfDesktopCanvas extends BaseElement {
       event.preventDefault();
 
       // Don't move the panel when it is already closed
-      if (!this.showDatapanel_) {
+      if (!this.showDatapanel_ && panelResizeEvent.leftElement.id === 'gmf-app-data-panel') {
         return;
       }
 
