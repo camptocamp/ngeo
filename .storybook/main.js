34b7925ea9ace2f17be70484e2b3371a90607b1d
Stéphane Brunner <stephane.brunner@camptocamp.com>
2026-07-21 21:44:28 +0000

    Exclude fontawesome SVGs from svgo-loader processing using webpack oneOf

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,24 @@
 // Storybook Webpack configuration.
 
+const path = require('path');
 const {merge} = require('webpack-merge');
 
-// Increase sax entity count limit for FontAwesome SVGs with many entities.
-// svgo 2.8.1+ migrated from @trysound/sax to upstream sax which enforces a 10,000 entity limit.
-const sax = require('sax');
-sax.MAX_ENTITY_COUNT = 20000;
+function increaseSaxEntityLimit() {
+  try {
+    const sax = require('sax');
+    sax.MAX_ENTITY_COUNT = 20000;
+  } catch (_e) {
+    try {
+      const svgoDir = path.dirname(require.resolve('svgo/package.json'));
+      const saxPath = path.join(svgoDir, 'node_modules', 'sax', 'lib', 'sax.js');
+      const sax = require(saxPath);
+      sax.MAX_ENTITY_COUNT = 20000;
+    } catch (_e2) {
+      // Unable to set sax MAX_ENTITY_COUNT; rely on webpack rule exclusion instead.
+    }
+  }
+}
+increaseSaxEntityLimit();
 
 module.exports = (env, args) => {
   const storybookConfig = {
@@ -29,6 +42,21 @@ module.exports = (env, args) => {
       // And add back the storybook one.
       const cssRule = config.module.rules.filter((f) => f.test.toString() === '/\\.css$/')[0];
       mergedConfig.module.rules.push(cssRule);
+
+      // Exclude fontawesome SVGs from svgo processing using oneOf to avoid cascading loaders.
+      // svgo 2.8.3 uses sax which enforces a 10,000 entity limit that FontAwesome SVGs exceed.
+      const currentRules = mergedConfig.module.rules;
+      mergedConfig.module.rules = [
+        {
+          oneOf: [
+            {
+              test: /@fortawesome\/.*\.svg$/,
+              use: 'file-loader',
+            },
+            ...currentRules,
+          ],
+        },
+      ];
 
       return mergedConfig;
     },
